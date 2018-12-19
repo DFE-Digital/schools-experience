27e9cbc5007a670bfee2ab4079be5f6df1d8f4b1
jebw <jeb@jdwilkins.co.uk>
2018-12-19 10:11:22 +0000

    Renamed app from SchoolExperienceCandidate to SchoolExperience

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "school_experience_candidate_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "school_experience_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 
