b794683e958661827ce9d5318c5f36ee72149fc4
jebw <jeb@jdwilkins.co.uk>
2018-12-20 15:42:33 +0000

    Added brakeman security check
    And removed dynamic page loading

--- Gemfile
+++ Gemfile
@@ -48,6 +48,8 @@ group :development, :test do
   gem 'rspec-rails', '~> 3.8'
 
   gem 'dotenv-rails'
+  
+  gem 'brakeman'
 end
 
 group :development do
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,15 @@
 class PagesController < ApplicationController
   def show
-    render template: "pages/#{params[:page]}"
+    render template: sanitise_page
+  end
+  
+  private
+  
+  def sanitise_page
+    case params[:page]
+    when 'home' then 'pages/home'
+    else
+      raise ActiveRecord::RecordNotFound
+    end
   end
 end
