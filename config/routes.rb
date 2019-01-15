33f0e3b37507fa3285673493bc7fa8fffa843ed6
jebw <jeb@jdwilkins.co.uk>
2019-01-15 09:51:30 +0000

    Added routes for interim screens for Candidates

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   get "/pages/:page", to: "pages#show"
+
+  namespace :candidate do
+    root to: 'home#index'
+
+    resources :schools, only: [:index, :show] do
+      get 'apply', to: 'placement_requests#new'
+      post 'apply', to: 'placement_requests#create'
+    end
+  end
 end
