2f9a12091265b63b437d03d54b4680027afd9e1b
jebw <jeb@jdwilkins.co.uk>
2019-01-15 17:24:24 +0000

    Renamed route from apply to request_placement

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
     root to: 'home#index'
 
     resources :schools, only: [:index, :show] do
-      get 'apply', to: 'placement_requests#new'
-      post 'apply', to: 'placement_requests#create'
+      get 'request_placement', to: 'placement_requests#new'
+      post 'request_placement', to: 'placement_requests#create'
     end
   end
 end
