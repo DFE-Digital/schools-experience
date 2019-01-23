e9aff6c71d043a05b5cf1f65554b215eac3728c9
Peter Yates <peter.yates@graphia.co.uk>
2019-01-23 16:06:27 +0000

    Remove unintentionally-added test tables

--- db/schema.rb
+++ db/schema.rb
@@ -25,41 +25,4 @@ ActiveRecord::Schema.define(version: 2019_01_22_103053) do
     t.index ["name"], name: "index_bookings_schools_on_name"
   end
 
-  create_table "departments", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "employee_skills", force: :cascade do |t|
-    t.integer "employee_id"
-    t.integer "skill_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "employees", force: :cascade do |t|
-    t.string "name"
-    t.string "telephone"
-    t.string "website"
-    t.string "email"
-    t.integer "department_id"
-    t.date "date_of_birth"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.boolean "is_at_least_18"
-    t.boolean "has_valid_passport"
-    t.boolean "has_clean_driving_licence"
-    t.string "is_at_least_18_details"
-    t.string "has_valid_passport_details"
-    t.string "has_clean_driving_licence_details"
-  end
-
-  create_table "skills", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
