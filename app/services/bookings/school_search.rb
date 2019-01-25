980c15966044b6dd1f1782d0c32d55dfc2694911
Peter Yates <peter.yates@graphia.co.uk>
2019-01-25 13:02:35 +0000

    Restructured Bookings::SchoolSearch, added sorting
    Now the service object is initialized with the search arguments and the
    results method actually executes the query. This was changed to cater
    for the additional step that only allows distance to be set as a sort
    option if we know where the candidate is.
    
    The search results also include a distance attribute (in metres!) that
    can be displayed with the results.

--- app/services/bookings/school_search.rb
+++ app/services/bookings/school_search.rb
@@ -1,4 +1,6 @@
 class Bookings::SchoolSearch
+  attr_accessor :query, :point, :radius, :subjects, :phases, :max_fee, :order
+
   # Search for schools based on a query string, location
   # and optional radius. Note, both +.search+ and +.close_to+
   # will not amend the +ActiveRecord::Relation+ if falsy values
@@ -7,23 +9,47 @@ class Bookings::SchoolSearch
   # This means if one is present and not the other, the query will fall
   # back to the present value. It also means that if none are passed,
   # all schools will be returned.
-  def search(query, location: nil, radius: 10, subject_ids: nil, phase_ids: nil, max_fee: nil)
+  def initialize(query, location: nil, radius: 10, subjects: nil, phases: nil, max_fee: nil, requested_order: 'distance')
+    self.query    = query
+    self.point    = extract_point(location)
+    self.radius   = radius
+    self.subjects = subjects
+    self.phases   = phases
+    self.max_fee  = max_fee
+    self.order    = order_by(requested_order)
+  end
+
+  def results
     Bookings::School
-      .search(query)
-      .close_to(point(location), radius: radius)
-      .that_provide(subject_ids)
-      .at_phases(phase_ids)
-      .costing_upto(max_fee)
+      .search(@query)
+      .close_to(@point, radius: @radius)
+      .that_provide(@subjects)
+      .at_phases(@phases)
+      .costing_upto(@max_fee)
+      .reorder(@order)
   end
 
 private
 
-  def point(location)
+  def extract_point(location)
     if (result = Geocoder.search(location)&.first)
       Bookings::School::GEOFACTORY.point(
         result.data.dig('lon'),
         result.data.dig('lat')
       )
+    end
+  end
+
+  def order_by(requested_order)
+    if (requested_order == 'distance') && @point.present?
+      # note distance isn't actually an attribute of
+      # Bookings::School so we can't use hash syntax
+      # as Rails will complain
+      'distance asc'
+    elsif requested_order == 'fee'
+      { fee: 'asc' }
+    else # default to alphabetical
+      { name: 'asc' }
     end
   end
 end
--- spec/services/bookings/school_search_spec.rb
+++ spec/services/bookings/school_search_spec.rb
@@ -1,197 +1,235 @@
 require 'rails_helper'
 
 describe Bookings::SchoolSearch do
-  describe '#search' do
-    before do
-      allow(Geocoder).to receive(:search).and_return([])
-    end
+  describe '#results' do
+    let(:manchester_coordinates) {
+      [
+        OpenStruct.new(data: { "lat" => "53.488", "lon" => "-2.242" }),
+        OpenStruct.new(data: { "lat" => "53.476", "lon" => "-2.229" })
+      ]
+    }
 
-    let(:point_in_manchester) { Bookings::School::GEOFACTORY.point(-2.241, 53.481) }
-    let(:point_in_leeds) { Bookings::School::GEOFACTORY.point(-1.548, 53.794) }
-
-    let!(:matching_school) do
-      create(
-        :bookings_school,
-        name: "Springfield Primary School",
-        coordinates: point_in_manchester,
-        fee: 10
-      )
-    end
-
-    let!(:non_matching_school) do
-      create(
-        :bookings_school,
-        name: "Pontefract Primary School",
-        coordinates: point_in_leeds,
-        fee: 30
-      )
-    end
-
-    context 'When no conditions are supplied' do
-      subject { Bookings::SchoolSearch.new.search('', location: '') }
-      specify 'results should include all schools' do
-        expect(subject.count).to eql(Bookings::School.count)
-      end
-    end
-
-    context 'When Geocoder finds a location' do
+    context 'Search Criteria' do
       before do
-        allow(Geocoder).to receive(:search).and_return(
-          [
-            OpenStruct.new(data: { "lat" => "53.488", "lon" => "-2.242" }),
-            OpenStruct.new(data: { "lat" => "53.476", "lon" => "-2.229" })
-          ]
+        allow(Geocoder).to receive(:search).and_return([])
+      end
+
+      let(:point_in_manchester) { Bookings::School::GEOFACTORY.point(-2.241, 53.481) }
+      let(:point_in_leeds) { Bookings::School::GEOFACTORY.point(-1.548, 53.794) }
+
+      let!(:matching_school) do
+        create(
+          :bookings_school,
+          name: "Springfield Primary School",
+          coordinates: point_in_manchester,
+          fee: 10
         )
       end
 
-      context 'When text and location are supplied' do
-        subject { Bookings::SchoolSearch.new.search('Springfield', location: 'Manchester') }
-
-        specify 'results should include matching records' do
-          expect(subject).to include(matching_school)
-        end
-
-        specify 'results should not include non-matching records' do
-          expect(subject).not_to include(non_matching_school)
-        end
-      end
-
-      context 'When only text is supplied' do
-        subject { Bookings::SchoolSearch.new.search('Springfield') }
-
-        let!(:matching_school) do
-          create(:bookings_school, name: "Springfield Primary School")
-        end
-
-        specify 'results should include matching records' do
-          expect(subject).to include(matching_school)
-        end
-
-        specify 'results should not include non-matching records' do
-          expect(subject).not_to include(non_matching_school)
-        end
-      end
-
-      context 'When only a location is supplied' do
-        subject { Bookings::SchoolSearch.new.search('', location: 'Manchester') }
-
-        let!(:matching_school) do
-          create(:bookings_school, name: "Springfield Primary School")
-        end
-
-        specify 'results should include matching records' do
-          expect(subject).to include(matching_school)
-        end
-
-        specify 'results should not include non-matching records' do
-          expect(subject).not_to include(non_matching_school)
-        end
-      end
-    end
-
-    context 'When GeoCoder finds no location' do
-      context 'When the query matches a school' do
-        subject { Bookings::SchoolSearch.new.search('Springfield', location: 'Madrid') }
-
-        specify 'results should include records that match the query' do
-          expect(subject).to include(matching_school)
-        end
-      end
-
-      context 'When the query does not match a school' do
-        subject { Bookings::SchoolSearch.new.search('William McKinley High', location: 'Chippewa, Michigan') }
-
-        specify 'results should include records that match the query' do
-          expect(subject).to be_empty
-        end
-      end
-    end
-
-    context 'Filtering' do
-      # subjects
-      let(:maths) { create(:bookings_subject, name: "Maths") }
-      let(:physics) { create(:bookings_subject, name: "Physics") }
-      # phases
-      let(:college) { create(:bookings_phase, name: "College") }
-      let(:secondary) { create(:bookings_phase, name: "Secondary") }
-
-      context 'Filtering on subjects' do
-        before do
-          matching_school.subjects << maths
-          non_matching_school.subjects << physics
-        end
-
-        subject { Bookings::SchoolSearch.new.search('', location: '', subject_ids: maths) }
-
-        specify 'should return matching results' do
-          expect(subject).to include(matching_school)
-        end
-
-        specify 'should omit non-matching results' do
-          expect(subject).not_to include(non_matching_school)
-        end
-      end
-
-      context 'Filtering on phases' do
-        before do
-          matching_school.phases << college
-          non_matching_school.phases << secondary
-        end
-
-        subject { Bookings::SchoolSearch.new.search('', location: '', phase_ids: college) }
-
-        specify 'should return matching results' do
-          expect(subject).to include(matching_school)
-        end
-
-        specify 'should omit non-matching results' do
-          expect(subject).not_to include(non_matching_school)
-        end
-      end
-
-      context 'Filtering on fees' do
-        subject { Bookings::SchoolSearch.new.search('', location: '', max_fee: 20) }
-
-        specify 'should return matching results' do
-          expect(subject).to include(matching_school)
-        end
-
-        specify 'should omit non-matching results' do
-          expect(subject).not_to include(non_matching_school)
-        end
-      end
-    end
-
-    context 'Chaining' do
-      let(:secondary) { create(:bookings_phase, name: "Secondary") }
-      let(:physics) { create(:bookings_subject, name: "Physics") }
-
-      before do
-        allow(Geocoder).to receive(:search).and_return(
-          [
-            OpenStruct.new(data: { "lat" => "53.488", "lon" => "-2.242" }),
-            OpenStruct.new(data: { "lat" => "53.476", "lon" => "-2.229" })
-          ]
+      let!(:non_matching_school) do
+        create(
+          :bookings_school,
+          name: "Pontefract Primary School",
+          coordinates: point_in_leeds,
+          fee: 30
         )
       end
 
-      before do
-        matching_school.phases << secondary
-        matching_school.subjects << physics
+      context 'When no conditions are supplied' do
+        subject { Bookings::SchoolSearch.new('', location: '').results }
+        specify 'results should include all schools' do
+          expect(subject.count).to eql(Bookings::School.count)
+        end
       end
 
-      subject do
-        Bookings::SchoolSearch.new.search(
-          'Springf',
-          location: 'Cheetham Hill',
-          subject_ids: physics,
-          phase_ids: secondary,
-          max_fee: 20
-        ).uniq
+      context 'When Geocoder finds a location' do
+        before do
+          allow(Geocoder).to receive(:search).and_return(manchester_coordinates)
+        end
+
+        context 'When text and location are supplied' do
+          subject { Bookings::SchoolSearch.new('Springfield', location: 'Manchester').results }
+
+          specify 'results should include matching records' do
+            expect(subject).to include(matching_school)
+          end
+
+          specify 'results should not include non-matching records' do
+            expect(subject).not_to include(non_matching_school)
+          end
+        end
+
+        context 'When only text is supplied' do
+          subject { Bookings::SchoolSearch.new('Springfield').results }
+
+          let!(:matching_school) do
+            create(:bookings_school, name: "Springfield Primary School")
+          end
+
+          specify 'results should include matching records' do
+            expect(subject).to include(matching_school)
+          end
+
+          specify 'results should not include non-matching records' do
+            expect(subject).not_to include(non_matching_school)
+          end
+        end
+
+        context 'When only a location is supplied' do
+          subject { Bookings::SchoolSearch.new('', location: 'Manchester').results }
+
+          let!(:matching_school) do
+            create(:bookings_school, name: "Springfield Primary School")
+          end
+
+          specify 'results should include matching records' do
+            expect(subject).to include(matching_school)
+          end
+
+          specify 'results should not include non-matching records' do
+            expect(subject).not_to include(non_matching_school)
+          end
+        end
       end
 
-      specify 'should allow all search options and filters to work in conjunction' do
-        expect(subject).to include(matching_school)
+      context 'When GeoCoder finds no location' do
+        context 'When the query matches a school' do
+          subject { Bookings::SchoolSearch.new('Springfield', location: 'Madrid').results }
+
+          specify 'results should include records that match the query' do
+            expect(subject).to include(matching_school)
+          end
+        end
+
+        context 'When the query does not match a school' do
+          subject { Bookings::SchoolSearch.new('William McKinley High', location: 'Chippewa, Michigan').results }
+
+          specify 'results should include records that match the query' do
+            expect(subject).to be_empty
+          end
+        end
+      end
+
+      context 'Filtering' do
+        # subjects
+        let(:maths) { create(:bookings_subject, name: "Maths") }
+        let(:physics) { create(:bookings_subject, name: "Physics") }
+        # phases
+        let(:college) { create(:bookings_phase, name: "College") }
+        let(:secondary) { create(:bookings_phase, name: "Secondary") }
+
+        context 'Filtering on subjects' do
+          before do
+            matching_school.subjects << maths
+            non_matching_school.subjects << physics
+          end
+
+          subject { Bookings::SchoolSearch.new('', location: '', subjects: maths).results }
+
+          specify 'should return matching results' do
+            expect(subject).to include(matching_school)
+          end
+
+          specify 'should omit non-matching results' do
+            expect(subject).not_to include(non_matching_school)
+          end
+        end
+
+        context 'Filtering on phases' do
+          before do
+            matching_school.phases << college
+            non_matching_school.phases << secondary
+          end
+
+          subject { Bookings::SchoolSearch.new('', location: '', phases: college).results }
+
+          specify 'should return matching results' do
+            expect(subject).to include(matching_school)
+          end
+
+          specify 'should omit non-matching results' do
+            expect(subject).not_to include(non_matching_school)
+          end
+        end
+
+        context 'Filtering on fees' do
+          subject { Bookings::SchoolSearch.new('', location: '', max_fee: 20).results }
+
+          specify 'should return matching results' do
+            expect(subject).to include(matching_school)
+          end
+
+          specify 'should omit non-matching results' do
+            expect(subject).not_to include(non_matching_school)
+          end
+        end
+      end
+
+      context 'Chaining' do
+        let(:secondary) { create(:bookings_phase, name: "Secondary") }
+        let(:physics) { create(:bookings_subject, name: "Physics") }
+
+        before do
+          allow(Geocoder).to receive(:search).and_return(manchester_coordinates)
+        end
+
+        before do
+          matching_school.phases << secondary
+          matching_school.subjects << physics
+        end
+
+        subject do
+          Bookings::SchoolSearch.new(
+            'Springf',
+            location: 'Cheetham Hill',
+            subjects: physics,
+            phases: secondary,
+            max_fee: 20
+          ).results
+        end
+
+        specify 'should allow all search options and filters to work in conjunction' do
+          expect(subject).to include(matching_school)
+        end
+      end
+    end
+
+    context 'Ordering' do
+      context 'Geographic ordering' do
+        let(:point_in_manchester) { Bookings::School::GEOFACTORY.point(-2.241, 53.481) }
+        let(:point_in_leeds) { Bookings::School::GEOFACTORY.point(-1.548, 53.794) }
+        let(:point_in_glasgow) { Bookings::School::GEOFACTORY.point(-4.219, 55.859) }
+        let(:point_in_york) { Bookings::School::GEOFACTORY.point(-1.095, 53.597) }
+
+        let!(:glasgow_school) { create(:bookings_school, name: "Glasgow", coordinates: point_in_glasgow) }
+        let!(:york_school) { create(:bookings_school, name: "York", coordinates: point_in_york) }
+        let!(:mcr_school) { create(:bookings_school, name: "Manchester", coordinates: point_in_manchester) }
+        let!(:leeds_school) { create(:bookings_school, name: "Leeds", coordinates: point_in_leeds) }
+
+        before do
+          allow(Geocoder).to receive(:search).and_return(manchester_coordinates)
+        end
+
+        subject do
+          Bookings::SchoolSearch.new('', location: 'Cheetham Hill', radius: 500, requested_order: 'distance').results
+        end
+
+        specify 'schools should be ordered by distance, near to far' do
+          expect(subject.map(&:name)).to eql([mcr_school, leeds_school, york_school, glasgow_school].map(&:name))
+        end
+      end
+      context 'Fee ordering' do
+        let!(:reasonable) { create(:bookings_school, name: "Cheap", fee: 15) }
+        let!(:cheap) { create(:bookings_school, name: "Cheap", fee: 0) }
+        let!(:expensive) { create(:bookings_school, name: "Cheap", fee: 30) }
+
+        subject do
+          Bookings::SchoolSearch.new('', requested_order: 'fee').results
+        end
+
+        specify 'schools should be ordered by fee, low to high' do
+          expect(subject.map(&:name)).to eql([cheap, reasonable, expensive].map(&:name))
+        end
       end
     end
   end
