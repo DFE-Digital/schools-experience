e7c19e99d76e088f1ecbbca4613124f27972f9b7
Peter Yates <peter.yates@graphia.co.uk>
2019-01-25 14:34:33 +0000

    Rename extract_point to geolocate

--- app/services/bookings/school_search.rb
+++ app/services/bookings/school_search.rb
@@ -3,7 +3,7 @@ class Bookings::SchoolSearch
 
   def initialize(query, location: nil, radius: 10, subjects: nil, phases: nil, max_fee: nil, requested_order: 'distance')
     self.query    = query
-    self.point    = extract_point(location)
+    self.point    = geolocate(location)
     self.radius   = radius
     self.subjects = subjects
     self.phases   = phases
@@ -26,7 +26,7 @@ class Bookings::SchoolSearch
 
 private
 
-  def extract_point(location)
+  def geolocate(location)
     if (result = Geocoder.search(location)&.first)
       Bookings::School::GEOFACTORY.point(
         result.data.dig('lon'),
