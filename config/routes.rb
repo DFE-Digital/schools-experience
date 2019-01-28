b4a9d6a8a28fe5641139c64af3dec90b1af7fada
jebw <jeb@jdwilkins.co.uk>
2019-01-28 15:12:45 +0000

    fixup! Changed namespace to Candidates

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get "/pages/:page", to: "pages#show"
-  root to: 'candidate/home#index'
+  root to: 'candidates/home#index'
 
   namespace :candidates do
     root to: 'home#index'
