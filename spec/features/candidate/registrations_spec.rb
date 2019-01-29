1f7f1e2eb091651215c9399affaf9b46478a4dbe
Richard Lynch <rlynch@yozu.co.uk>
2019-01-29 13:58:09 +0000

    SE-168 #comment styles initial registraion screens
    Styles the initial registration screens, using the
    govuk_elements_form_builder.

--- spec/features/candidate/registrations_spec.rb
+++ spec/features/candidate/registrations_spec.rb
@@ -18,21 +18,42 @@ feature 'Candidate Registrations', type: :feature do
   scenario 'Submit registrations/placements form with errors' do
     visit '/candidate/registrations/placements/new'
 
-    fill_in 'Start Date', with: today
-    fill_in 'End Date',   with: tomorrow
-    fill_in 'Objectives', with: 'I enjoy teaching'
+    within all('.govuk-date-input')[0] do
+      fill_in 'Day',   with: today.day
+      fill_in 'Month', with: today.month
+      fill_in 'Year',  with: today.year
+    end
+
+    within all('.govuk-date-input')[1] do
+      fill_in 'Day',   with: tomorrow.day
+      fill_in 'Month', with: tomorrow.month
+      fill_in 'Year',  with: tomorrow.year
+    end
+
+    fill_in 'Explain in 50 words or fewer', with: 'I enjoy teaching'
 
     click_button 'Continue'
 
-    expect(page).to have_text 'Please select an option'
+    expect(page).to have_text 'There is a problem'
   end
 
   scenario 'Submit registrations/placements form successfully' do
     visit '/candidate/registrations/placements/new'
 
-    fill_in 'Start Date', with: today
-    fill_in 'End Date',   with: tomorrow
-    fill_in 'Objectives', with: 'I enjoy teaching'
+    within all('.govuk-date-input')[0] do
+      fill_in 'Day',   with: today.day
+      fill_in 'Month', with: today.month
+      fill_in 'Year',  with: today.year
+    end
+
+    within all('.govuk-date-input')[1] do
+      fill_in 'Day',   with: tomorrow.day
+      fill_in 'Month', with: tomorrow.month
+      fill_in 'Year',  with: tomorrow.year
+    end
+
+    fill_in 'Explain in 50 words or fewer', with: 'I enjoy teaching'
+
     choose 'No'
 
     click_button 'Continue'
@@ -51,7 +72,7 @@ feature 'Candidate Registrations', type: :feature do
 
     click_button 'Continue'
 
-    expect(page).to have_text "can't be blank"
+    expect(page).to have_text "There is a problem"
   end
 
   scenario 'Submit registrations/personal_details form successfully' do
@@ -62,7 +83,7 @@ feature 'Candidate Registrations', type: :feature do
     fill_in 'Town or city', with: 'Test Town'
     fill_in 'County', with: 'Testshire'
     fill_in 'Postcode', with: 'TE57 1NG'
-    fill_in 'Phone', with: '01234567890'
+    fill_in 'UK telephone number', with: '01234567890'
 
     click_button 'Continue'
 
@@ -73,23 +94,23 @@ feature 'Candidate Registrations', type: :feature do
     visit '/candidate/registrations/account_infos/new'
 
     choose 'Graduate or postgraduate'
-    select 'Physics', from: 'Degree subject'
+    select 'Physics', from: 'Select the nearest or equivalent option'
     choose 'I want to become a teacher'
-    select 'Physics', from: 'Subject first choice'
+    select 'Physics', from: 'First choice'
 
     click_button 'Continue'
 
-    expect(page).to have_text "can't be blank"
+    expect(page).to have_text "There is a problem"
   end
 
   scenario 'Submit registrations/account-info form successfully' do
     visit '/candidate/registrations/account_infos/new'
 
     choose 'Graduate or postgraduate'
-    select 'Physics', from: 'Degree subject'
+    select 'Physics', from: 'Select the nearest or equivalent option'
     choose 'I want to become a teacher'
-    select 'Physics', from: 'Subject first choice'
-    select 'Mathematics', from: 'Subject second choice'
+    select 'Physics', from: 'First choice'
+    select 'Mathematics', from: 'Second choice'
 
     click_button 'Continue'
 
