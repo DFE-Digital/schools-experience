db5a87e7f315801df939efbed9dc0289dcd0fcec
Peter Yates <peter.yates@graphia.co.uk>
2019-01-31 13:58:58 +0000

    Add Phases to seed
    This data was extracted from EduBase. Note that 'Not applicable' has a
    pk of zero so to keep the keys in sync it is manually accounted for. If
    we hardcoded all of the IDs the sequence would clash if we add any more
    phases.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,15 @@
 # Subjects
 YAML.load_file(File.join(Rails.root, 'db', 'data', 'subjects.yml')).each do |name|
-  Bookings::Subject.create(name: name)
+end
+
+# Phases
+YAML.load_file(File.join(Rails.root, 'db', 'data', 'phases.yml')).each do |id, name|
+  # Force the 'Not applicable' pk to be zero soit matches the EduBase value,
+  # allow auto number to assign the rest (they are in order so should match and
+  # keep the sequence in sync)
+  if id.zero?
+    Bookings::Phase.create(id: id, name: name)
+  else
+    Bookings::Phase.create(name: name)
+  end
 end
