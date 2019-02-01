f72c1566bad9f5be8a422c5ad7ef430862987e9d
Richard Lynch <rlynch@yozu.co.uk>
2019-02-01 16:20:42 +0000

    fix param name

--- app/controllers/candidate/registrations/subject_preferences_controller.rb
+++ app/controllers/candidate/registrations/subject_preferences_controller.rb
@@ -6,7 +6,7 @@ module Candidate
       end
 
       def create
-        @subject_preference = SubjectPreference.new subject_preference
+        @subject_preference = SubjectPreference.new subject_preference_params
         if @subject_preference.valid?
           current_registration[:subject_preference] = @subject_preference.attributes
           redirect_to new_candidate_registrations_background_check_path
@@ -17,7 +17,7 @@ module Candidate
 
     private
 
-      def subject_preference
+      def subject_preference_params
         params.require(:candidate_registrations_subject_preference).permit \
           :degree_stage,
           :degree_stage_explaination,
