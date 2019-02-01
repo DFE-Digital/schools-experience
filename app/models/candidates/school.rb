2bbbc67e9015583d3af644c9edb6fbda3cf937bb
jebw <jeb@jdwilkins.co.uk>
2019-02-01 11:45:31 +0000

    Added School service model
    Allows retrieving a School, plus lists of all Subjects and Phases known to the
    Bookings service

---
+++ app/models/candidates/school.rb
@@ -0,0 +1,20 @@
+class Candidates::School
+  class << self
+    def find(identifier)
+      # Note currently using id, will be converted to URN when available
+      Bookings::School.find_by(id: identifier)
+    end
+
+    def phases
+      Bookings::Phase.all.map do |phase|
+        [phase.id, phase.name]
+      end
+    end
+
+    def subjects
+      Bookings::Subject.order(name: :asc).map do |subject|
+        [subject.id, subject.name]
+      end
+    end
+  end
+end
---
+++ spec/models/candidates/school_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+describe Candidates::School do
+  context '.find' do
+    before { @school = create(:bookings_school) }
+
+    context('with valid identifier') do
+      it "will return school" do
+        expect(described_class.find(@school.to_param)).to eq(@school)
+      end
+    end
+
+    context('with invalid URN') do
+      it "will return nil" do
+        expect(described_class.find('abc123')).to be_nil
+      end
+    end
+  end
+
+  context '.phases' do
+    before do
+      @third = create(:bookings_phase, name: 'third', position: 3)
+      @first = create(:bookings_phase, name: 'first', position: 1)
+      @second = create(:bookings_phase, name: 'second', position: 2)
+
+      @phases = described_class.phases
+    end
+
+    it 'should return an array of Phases ordered by the age period' do
+      expect(@phases).to eq([
+        [@first.id, @first.name],
+        [@second.id, @second.name],
+        [@third.id, @third.name]
+      ])
+    end
+  end
+
+  context '.subjects' do
+    before do
+      @later = create(:bookings_subject, name: 'Later')
+      @earlier = create(:bookings_subject, name: 'Earlier')
+      @subjects = described_class.subjects
+    end
+
+    it "should return a alphabetical array of subjects" do
+      expect(@subjects).to eq([
+        [@earlier.id, @earlier.name],
+        [@later.id, @later.name]
+      ])
+    end
+  end
+end
