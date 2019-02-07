7fe00103b1ef22f062323bdecb882f93a60dc2b4
Richard Lynch <rlynchdev@gmail.com>
2019-02-07 14:34:08 +0000

    Add paths to 'Change' link

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,11 @@ Rails.application.routes.draw do
 
     resources :schools, only: %i{index show} do
       namespace :registrations do
-        resource :placement_preference, only: %i(new create)
-        resource :account_check, only: %i(new create)
-        resource :address, only: %i(new create)
-        resource :subject_preference, only: %i(new create)
-        resource :background_check, only: %i(new create)
+        resource :placement_preference, only: %i(new create edit)
+        resource :account_check, only: %i(new create edit update)
+        resource :address, only: %i(new create edit)
+        resource :subject_preference, only: %i(new create edit)
+        resource :background_check, only: %i(new create edit)
         resource :application_preview, only: %i(show)
         resource :placement_request, only: %i(show create)
       end
