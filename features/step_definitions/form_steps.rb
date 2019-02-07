5fdd3cfbd17d7700b8d4176cdc1c7f738d32812a
Peter Yates <peter.yates@graphia.co.uk>
2019-02-07 15:00:09 +0000

    Update formbuilder to pull in fieldset heading
    This change breaks a few features that are checking for labels, so
    update them appropriately

--- features/step_definitions/form_steps.rb
+++ features/step_definitions/form_steps.rb
@@ -69,7 +69,11 @@ Given("I choose {string} from the {string} radio buttons") do |option, field|
 end
 
 def get_form_group(page, label_text)
-  page.find(".govuk-label", text: label_text).ancestor('div.govuk-form-group')
+  if page.has_css?(".govuk-label", text: label_text)
+    page.find(".govuk-label", text: label_text).ancestor('div.govuk-form-group')
+  else
+    page.find("legend", text: label_text).ancestor('div.govuk-form-group')
+  end
 end
 
 def ensure_date_field_exists(form_group)
