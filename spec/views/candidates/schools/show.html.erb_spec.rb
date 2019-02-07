8253c1e06665a79ea2ea44cfc070196d9483f1e8
jebw <jeb@jdwilkins.co.uk>
2019-02-07 11:49:16 +0000

    Added correct link on Show School Profile page

--- spec/views/candidates/schools/show.html.erb_spec.rb
+++ spec/views/candidates/schools/show.html.erb_spec.rb
@@ -11,6 +11,9 @@ RSpec.describe "candidates/schools/show.html.erb", type: :view do
   end
 
   it "will include the schools name" do
+    link = candidates_school_registrations_placement_preference_path(demo_school.urn)
+
     expect(rendered).to have_css('h1', text: demo_school.name)
+    expect(rendered).to have_css("a.govuk-button[href=\"#{link}\"]")
   end
 end
