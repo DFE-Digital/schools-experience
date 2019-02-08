e7898a8906d6ff38ae460d296b0ffca82d1e8b6d
Peter Fry <peterfry@peters-mbp.mynet>
2019-02-08 09:05:14 +0000

    register local selenium drivers

--- features/support/env.rb
+++ features/support/env.rb
@@ -72,3 +72,20 @@ if ENV['SELENIUM_HUB_HOSTNAME'].present?
         :desired_capabilities => :chrome)
   end
 end
+
+if ENV['BROWSER'] == 'chrome'
+  Capybara.default_driver = :chrome
+  Capybara.register_driver :chrome do |app|
+    Capybara::Selenium::Driver.new(app, browser: :chrome)
+  end
+elsif ENV['BROWSER'] == 'firefox'
+  Capybara.default_driver = :firefox
+  Capybara.register_driver :firefox do |app|
+    Capybara::Selenium::Driver.new(app, browser: :firefox)
+  end
+elsif ENV['BROWSER'] == 'chrome'
+  Capybara.default_driver = :ie
+  Capybara.register_driver :ie do |app|
+    Capybara::Selenium::Driver.new(app, browser: :internet_explorer)
+  end
+end
