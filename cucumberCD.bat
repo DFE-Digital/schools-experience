d712144f3ecd02f9042b91763a18a83d78123e66
Peter Fry <peterfry@peters-mbp.mynet>
2019-02-08 12:33:20 +0000

    DOS nonsense

--- cucumberCD.bat
+++ cucumberCD.bat
@@ -1,11 +1,11 @@
 call gem install bundler -v 1.17.3 
 call bundle install 
-call set CUC_DB_HOST=%DATABASEHOST% 
-call set CUC_DB_DATABASE=%DATABASENAME% 
-call set CUC_DB_USERNAME=%DATABASEUSER% 
-call set CUC_DB_PASSWORD=%PSWD% 
-call set BROWSER=chrome 
-call set SKIP_REDIS=true 
-call set DATABASE_CLEANER_ALLOW_REMOTE_DATABASE_URL=true
-call set RAILS_ENV=test 
+set CUC_DB_HOST=%DATABASEHOST% 
+set CUC_DB_DATABASE=%DATABASENAME% 
+set CUC_DB_USERNAME=%DATABASEUSER% 
+set CUC_DB_PASSWORD=%PSWD% 
+set BROWSER=chrome 
+set SKIP_REDIS=true 
+set DATABASE_CLEANER_ALLOW_REMOTE_DATABASE_URL=true
+set RAILS_ENV=test 
 call cucumber
