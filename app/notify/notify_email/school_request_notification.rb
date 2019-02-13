db8b38f6e07b608f7dd82f00b58d822adf719233
Peter Yates <peter.yates@graphia.co.uk>
2019-02-13 15:37:17 +0000

    Added school request notification email

---
+++ app/notify/notify_email/school_request_notification.rb
@@ -0,0 +1,89 @@
+class NotifyEmail::SchoolRequestNotification < Notify
+  attr_accessor :candidate_address,
+    :candidate_dbs_check_document,
+    :candidate_degree_stage,
+    :candidate_degree_subject,
+    :candidate_disability_needs,
+    :candidate_email_address,
+    :candidate_name,
+    :candidate_phone_number,
+    :candidate_teaching_stage,
+    :candidate_teaching_subject_first_choice,
+    :candidate_teaching_subject_second_choice,
+    :placement_finish_date,
+    :placement_outcome,
+    :placement_start_date,
+    :school_name,
+    :school_admin_name
+
+  def initialize(
+    to:,
+    candidate_address:,
+    candidate_dbs_check_document:,
+    candidate_degree_stage:,
+    candidate_degree_subject:,
+    candidate_disability_needs:,
+    candidate_email_address:,
+    candidate_name:,
+    candidate_phone_number:,
+    candidate_teaching_stage:,
+    candidate_teaching_subject_first_choice:,
+    candidate_teaching_subject_second_choice:,
+    placement_finish_date:,
+    placement_outcome:,
+    placement_start_date:,
+    school_name:,
+    school_admin_name:
+  )
+
+    self.candidate_address                        =        candidate_address
+    self.candidate_dbs_check_document             =        candidate_dbs_check_document
+    self.candidate_degree_stage                   =        candidate_degree_stage
+    self.candidate_degree_subject                 =        candidate_degree_subject
+    self.candidate_disability_needs               =        candidate_disability_needs
+    self.candidate_email_address                  =        candidate_email_address
+    self.candidate_name                           =        candidate_name
+    self.candidate_phone_number                   =        candidate_phone_number
+    self.candidate_teaching_stage                 =        candidate_teaching_stage
+    self.candidate_teaching_subject_first_choice  =        candidate_teaching_subject_first_choice
+    self.candidate_teaching_subject_second_choice =        candidate_teaching_subject_second_choice
+    self.placement_finish_date                    =        placement_finish_date
+    self.placement_outcome                        =        placement_outcome
+    self.placement_start_date                     =        placement_start_date
+    self.school_name                              =        school_name
+    self.school_admin_name                        =        school_admin_name
+
+    super(to: to)
+  end
+
+  def self.from_registration_session(application_preview, uuid)
+    # TODO for ease, allow initialisation via a RegistrationSession
+  end
+
+private
+
+  def template_id
+    'dd4490f8-1d7b-455b-8502-47f57a65179a'
+  end
+
+  def personalisation
+    {
+      candidate_address: candidate_address,
+      candidate_dbs_check_document: candidate_dbs_check_document,
+      candidate_degree_stage: candidate_degree_stage,
+      candidate_degree_subject: candidate_degree_subject,
+      candidate_disability_needs: candidate_disability_needs,
+      candidate_email_address: candidate_email_address,
+      candidate_name: candidate_name,
+      candidate_phone_number: candidate_phone_number,
+      candidate_teaching_stage: candidate_teaching_stage,
+      candidate_teaching_subject_first_choice: candidate_teaching_subject_first_choice,
+      candidate_teaching_subject_second_choice: candidate_teaching_subject_second_choice,
+      placement_finish_date: placement_finish_date,
+      placement_outcome: placement_outcome,
+      placement_start_date: placement_start_date,
+      school_name: school_name,
+      school_admin_name: school_admin_name
+    }
+  end
+end
---
+++ spec/notify/notify_email/school_request_notification_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe NotifyEmail::SchoolRequestNotification do
+  it_should_behave_like "email template", "dd4490f8-1d7b-455b-8502-47f57a65179a",
+    candidate_address: "392 Firwood Crescent",
+    candidate_dbs_check_document: "Yes",
+    candidate_degree_stage: "Postgraduate",
+    candidate_degree_subject: "Sociology",
+    candidate_disability_needs: "None",
+    candidate_email_address: "milhouse.vh@gmail.com",
+    candidate_name: "Milhouse van Houten",
+    candidate_phone_number: "01234 456 678",
+    candidate_teaching_stage: "I want to become a teacher",
+    candidate_teaching_subject_first_choice: "Sociology",
+    candidate_teaching_subject_second_choice: "Philosophy",
+    placement_finish_date: "2020-01-14",
+    placement_outcome: "I enjoy teaching",
+    placement_start_date: "2020-01-07",
+    school_name: "Springfield Elementary School",
+    school_admin_name: "Seymour Skinner"
+end
