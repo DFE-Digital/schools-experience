51a674ca7c02f1c550bf67b32ca7cfa49533e8bd
Peter Fry <peterfry@Peters-MacBook-Pro.local>
2019-02-18 16:41:00 +0000

    refining script

--- script/check_timings.py
+++ script/check_timings.py
@@ -1,4 +1,4 @@
-# A python script has been written which processes the cucumber json output. It takes two arguments, the first argument 
+# A python script has been written which processes the cucumber json output. It takes two arguments, the first argument
 # is the location of the cucumber test result json file and the second is the minimum response time in nano seconds.
 #
 # Usage:
@@ -8,7 +8,7 @@
 
 import json
 import sys
-  
+
 with open(sys.argv[1]) as data_file:
     json_data = json.load(data_file)
 
@@ -16,13 +16,17 @@ failures = {}
 
 max_duration = int(sys.argv[2])
 
+print('ignoring first failure which is always slow for various reasons (starting up browser via driver etc.)')
+index=0
+
 for feature in json_data:
   for scenario in feature['elements']:
     if scenario['type'] == 'scenario':
       for step in scenario['steps']:
         if step['result']['status'] == 'passed':
-          if step['result']['duration'] > max_duration:
+          if step['result']['duration'] > max_duration and index > 0:
             failures[feature['id'] + ' ' + scenario['id'] + ' ' + step['name']] = step['result']['duration']
+          index = index + 1
 
 ninezeros = 1000000000
 
@@ -30,5 +34,5 @@ for key, value in failures.items():
   print("Duration {:.2f}s > {:.2f} {}".format( value/ninezeros, max_duration/ninezeros, key))
 
 if len(failures) > 0:
-  print("ERROR: some responses were too slow.") 
+  print("ERROR: some responses were too slow.")
   sys.exit(1)
