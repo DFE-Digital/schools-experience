c0b685f255dfc55fef6a77e266e1f772cb1ddf9b
Richard Lynch <rlynchdev@gmail.com>
2019-02-22 16:35:01 +0000

    Update feature specs
    Updates feature specs now that we've replaced account check and address
    with contact information

--- features/step_definitions/candidates/registrations/unfortunately_we_dont_have_your_details_steps.rb
+++
@@ -1,5 +0,0 @@
-Then("there should be some information explaining why my contact details are needed") do
-  expect(page).to have_content(
-    "Register your details with us so you can get school experience placements"
-  )
-end
