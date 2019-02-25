9501a3d946793ec92472e06580cd5b03e49c0f1f
Peter Yates <peter.yates@graphia.co.uk>
2019-02-25 13:38:18 +0000

    Configure Kaminari to match the prototype desgin

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -2,10 +2,10 @@
 Kaminari.configure do |config|
   config.default_per_page = 20
   # config.max_per_page = nil
-  # config.window = 4
-  # config.outer_window = 0
-  # config.left = 0
-  # config.right = 0
+  config.window = 4
+  config.outer_window = 5
+  config.left = 5
+  config.right = 5
   # config.page_method_name = :page
   # config.param_name = :page
   # config.params_on_first_page = false
