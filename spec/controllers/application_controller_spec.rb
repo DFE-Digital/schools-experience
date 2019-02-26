57683901cb9d399ecd1c83b2b14503bb539832cc
Peter Yates <peter.yates@graphia.co.uk>
2019-02-26 12:18:28 +0000

    Add X-Robots-Tag header to all responses
    It will be set to 'all' for crawlable paths and 'none' for everything
    else. Currently the only crawlable paths are '/' and '/candidates'.
    
    https://developers.google.com/search/reference/robots_meta_tag

---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe ApplicationController, type: :request do
+  describe 'X-Robots-Tag' do
+    let(:header) { 'X-Robots-Tag' }
+    let(:school) { create(:bookings_school) }
+
+    specify "should add 'X-Robots-Tag: all' to crawlable paths" do
+      described_class::CRAWLABLE_PATHS.each do |cp|
+        head cp
+        expect(response.headers[header]).to eql('all')
+      end
+    end
+
+    specify "should add 'X-Robots-Tag: none' to uncrawlable paths" do
+      [
+        "/healthcheck.txt",
+        candidates_schools_path,
+        candidates_school_path(school),
+        new_candidates_school_registrations_subject_preference_path(school),
+        new_candidates_school_registrations_account_check_path(school)
+
+      ].each do |cp|
+        head cp
+        expect(response.headers[header]).to eql('none')
+      end
+    end
+  end
+end
