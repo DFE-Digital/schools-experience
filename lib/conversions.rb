ed5a468653f6c7fa31e3a82fe37708eaef3f4caa
jebw <jeb@jdwilkins.co.uk>
2019-03-07 12:24:57 +0000

    Added showing distances on School Search results

--- lib/conversions.rb
+++ lib/conversions.rb
@@ -1,9 +1,29 @@
 module Conversions
   module Distance
+    METRES_IN_MILE = 1609.344
+
     module Miles
       module ToMetres
         def self.convert(miles)
-          miles * 1609.344
+          miles * METRES_IN_MILE
+        end
+      end
+    end
+
+    module Metres
+      module ToMiles
+        def self.convert(metres, precision = 1)
+          if precision.nil?
+            metres.to_f / METRES_IN_MILE
+          elsif precision.zero?
+            (metres.to_f / METRES_IN_MILE).
+              to_s(:rounded, precision: 0).
+              to_i
+          else
+            (metres.to_f / METRES_IN_MILE).
+              to_s(:rounded, precision: precision).
+              to_f
+          end
         end
       end
     end
--- spec/lib/conversions_spec.rb
+++ spec/lib/conversions_spec.rb
@@ -14,4 +14,50 @@ describe 'Conversions' do
       end
     end
   end
+
+  describe 'Distance::Miles::ToMetres' do
+    context '.convert' do
+      context 'with precision of 0' do
+        subject { Conversions::Distance::Metres::ToMiles }
+
+        specify "should divide by 1609.344" do
+          {
+            1800 => 1,
+            3600 => 2,
+            4700 => 3,
+          }.each do |metres, miles|
+            expect(subject.convert(metres, 0)).to eql(miles)
+          end
+        end
+      end
+
+      context 'with precision of 1' do
+        subject { Conversions::Distance::Metres::ToMiles }
+
+        specify "should divide by 1609.344" do
+          {
+            1800 => 1.1,
+            3600 => 2.2,
+            4700 => 2.9,
+          }.each do |metres, miles|
+            expect(subject.convert(metres)).to eql(miles)
+          end
+        end
+      end
+
+      context 'with infinite precision' do
+        subject { Conversions::Distance::Metres::ToMiles }
+
+        specify "should divide by 1609.344" do
+          {
+            1800 => 1.1184,
+            3600 => 2.2369,
+            4700 => 2.9204,
+          }.each do |metres, miles|
+            expect(subject.convert(metres, nil)).to be_within(0.0001).of(miles)
+          end
+        end
+      end
+    end
+  end
 end
