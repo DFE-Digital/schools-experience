501a3c8420e7101792fcfcad96a81efe67f05cd4
Richard Lynch <rlynchdev@gmail.com>
2019-03-08 13:48:22 +0000

    Moves placement_request model under bookings namespace

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_26_120359) do
+ActiveRecord::Schema.define(version: 2019_03_08_132741) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,21 @@ ActiveRecord::Schema.define(version: 2019_02_26_120359) do
     t.integer "edubase_id"
     t.index ["name"], name: "index_bookings_phases_on_name"
     t.index ["position"], name: "index_bookings_phases_on_position", unique: true
+  end
+
+  create_table "bookings_placement_requests", force: :cascade do |t|
+    t.text "objectives", null: false
+    t.integer "urn", null: false
+    t.string "degree_stage", null: false
+    t.text "degree_stage_explaination"
+    t.string "degree_subject", null: false
+    t.string "teaching_stage", null: false
+    t.string "subject_first_choice", null: false
+    t.string "subject_second_choice", null: false
+    t.boolean "has_dbs_check", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "availability", null: false
   end
 
   create_table "bookings_school_types", force: :cascade do |t|
@@ -74,21 +89,6 @@ ActiveRecord::Schema.define(version: 2019_02_26_120359) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_bookings_subjects_on_name", unique: true
-  end
-
-  create_table "candidates_registrations_placement_requests", force: :cascade do |t|
-    t.text "objectives", null: false
-    t.integer "urn", null: false
-    t.string "degree_stage", null: false
-    t.text "degree_stage_explaination"
-    t.string "degree_subject", null: false
-    t.string "teaching_stage", null: false
-    t.string "subject_first_choice", null: false
-    t.string "subject_second_choice", null: false
-    t.boolean "has_dbs_check", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.text "availability", null: false
   end
 
   create_table "delayed_jobs", force: :cascade do |t|
