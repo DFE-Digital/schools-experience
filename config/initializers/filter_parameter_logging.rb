f10b051ba2d255dcb99c8251aa1cab1efcf27454
Richard Lynch <rlynchdev@gmail.com>
2019-03-19 10:11:35 +0000

    Scrub pii from params
    Add params from candidate contact_information controller to filter
    parameters list

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,14 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password]
+Rails.application.config.filter_parameters += [
+  :password,
+  :full_name,
+  :email,
+  :building,
+  :street,
+  :town_or_city,
+  :county,
+  :postcode,
+  :phone
+]
