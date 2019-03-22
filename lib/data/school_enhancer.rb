0dd93e28396c40ff378128623d5f5012ba9202fe
Peter Yates <peter.yates@graphia.co.uk>
2019-03-22 17:21:22 +0000

    Add cleanup functionality for text and websites
    The three main datasets have varying quality data. These changes
    attempt to make the imported data neater and more consistent.
    
    Websites are helped towards validity by prepending them with 'http://'
    if it's missing and text is optimised for Rails' `simple_format` by
    splitting paragraphs out properly, fixing dates, removing odd characters
    and stripping out non-Unix line endings.
    
    The resulting import isn't perfect but is much improved over the raw
    data.

--- lib/data/school_enhancer.rb
+++ lib/data/school_enhancer.rb
@@ -35,20 +35,22 @@ class SchoolEnhancer
           next
         end
 
-        school.update_attributes(
-          availability_info: cleanse_text(row['school_experience_availability_details']),
-          placement_info: cleanse_text(row['school_experience_description']),
-          primary_key_stage_info: cleanse_text(row['primary_key_stage_details']),
-          teacher_training_info: cleanse_text(row['teacher_training_details']),
+        if school.update_attributes(
+          availability_info: format_text(row['school_experience_availability_details']),
+          placement_info: format_text(row['school_experience_description']),
+          primary_key_stage_info: format_text(row['primary_key_stage_details']),
+          teacher_training_info: format_text(row['teacher_training_details']),
           teacher_training_provider: row['teacher_training_details'].present?,
-          teacher_training_website: row['itt_website']
+          teacher_training_website: cleanup_website(urn, row['itt_website'])
         )
+          if row['secondary_subjects'].present?
+            school.subjects << extract_secondary_subjects(row['secondary_subjects'])
+          end
 
-        if row['secondary_subjects'].present?
-          school.subjects << extract_secondary_subjects(row['secondary_subjects'])
+          puts "#{i} of #{total} #{school.name} enhanced"
+        else
+          fail "#{i} of #{total} #{school.name} failed"
         end
-
-        puts "#{i} of #{total} #{school.name} enhanced"
       end
     end
   end
@@ -67,7 +69,44 @@ private
     secondary_subjects.values_at(*raw.split(', ')).compact
   end
 
-  def cleanse_text(raw)
-    raw.remove("\r").strip if raw.present?
+  def cleanup_website(urn, url)
+    return nil unless url.present?
+    return nil if url =~ /@/ # we don't want auth info or email addresses
+
+    first_url = url.split(" ").first
+
+    url_with_prefix = if first_url.starts_with?("http")
+                        first_url
+                      else
+                        "http://#{first_url}"
+                      end
+
+    if url_with_prefix.match?(URI::regexp(%w(http https)))
+      url_with_prefix.downcase
+    else
+      Rails.logger.warn("invalid website for #{urn}, #{url}")
+      nil
+    end
+  end
+
+  def format_text(raw)
+    return nil unless raw.present?
+
+    raw
+      .remove("***")                          # remove triple 'highlighting' asterisks
+      .gsub("  ", " ")                        # replace double spaces with single
+      .remove("\r")                           # remove non-unix return chars
+      .remove("•")                            # list char
+      .lines
+      .map(&:strip)                           # strip off any leading and trailing whitespace
+      .each { |l| !l.blank? && l[0] = l[0]&.upcase } # capitalize new paragraphs without affecting subsequent sentences
+      .join("\n\n")
+      .gsub(/(\d{2})\.(\d{2})\.(\d{2,4})/) {  # fix date format from 01.02.03 to 01/02/03
+        "%<day>s/%<month>s/%<year>s" % {      # note we're using string so we needn't worry about padding zeroes
+          day: $1,
+          month: $2,
+          year: $3
+        }
+      }
   end
 end
