dc1a24a831194d3d87ab5f65f7a2feccc69775b2
Peter Yates <peter.yates@graphia.co.uk>
2019-03-27 19:24:59 +0000

    Add SchoolManager class and enable/disable tasks

---
+++ lib/data/school_manager.rb
@@ -0,0 +1,29 @@
+class SchoolManager
+  attr_accessor :urns
+
+  def initialize(urns)
+    self.urns = urns
+  end
+
+  def enable_urns
+    set_enabled(true)
+  end
+
+  def disable_urns
+    set_enabled(false)
+  end
+
+private
+
+  def set_enabled(enabled)
+    Bookings::School.transaction do
+      urns.each do |row|
+        Bookings::School.find_by(urn: row['urn']).tap do |bs|
+          fail "no school found with urn #{urn}" unless bs.present?
+          puts "Updating #{bs.name}, enabled: #{enabled}"
+          bs.update_attributes(enabled: enabled)
+        end
+      end
+    end
+  end
+end
