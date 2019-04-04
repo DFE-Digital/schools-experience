e4cd1b4473a40dfa8b6ac0a836b16ccdcd14643f
Peter Yates <peter.yates@graphia.co.uk>
2019-04-04 10:09:09 +0100

    Remove Schools::User and Schools::School models
    Also update tests accordingly

--- spec/controllers/schools/dashboards_controller_spec.rb
+++ spec/controllers/schools/dashboards_controller_spec.rb
@@ -5,13 +5,16 @@ describe Schools::DashboardsController, type: :request do
   context '#show' do
     include_context "logged in DfE user"
 
+    let!(:school) do
+      FactoryBot.create(:bookings_school, name: 'organisation one', urn: '356127')
+    end
+
     before do
       get '/schools/dashboard'
     end
 
     it 'sets the correct school' do
-      expect(assigns(:school)).to eq \
-        Schools::School.new name: 'organisation one', urn: '356127'
+      expect(assigns(:school)).to eq(school)
     end
 
     it 'renders the show template' do
