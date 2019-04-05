c67e4e634e7d3b4846e734395f788340d8e9351e
Peter Yates <peter.yates@graphia.co.uk>
2019-04-05 14:03:45 +0100

    Attempt to fix the healthcheck in CI
    For DfE signin to work the application needs to run with SSL but since
    the changes were made to the puma config it's been failing due to curl
    receiving an empty reply due to the following error:
    
     SSL error, peer: 127.0.0.1, peer cert: , #<Puma::MiniSSL::SSLError:
     OpenSSL error: error:1407609C:SSL routines:SSL23_GET_CLIENT_HELLO:http
     request - 336027804>

--- bin/check_health.sh
+++ bin/check_health.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [[ -z "${SECURE_USERNAME}" ]]; then
-  exec curl --fail http://localhost:3000/
+  exec curl --insecure --fail https://localhost:3000/
 else
-  exec curl --user $SECURE_USERNAME:$SECURE_PASSWORD --fail http://localhost:3000/
+  exec curl --insecure --user $SECURE_USERNAME:$SECURE_PASSWORD --fail https://localhost:3000/
 fi
