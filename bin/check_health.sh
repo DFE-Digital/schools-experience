a5b345f9f36e0d99b0395bbad785f7574e056edd
Peter Yates <peter.yates@graphia.co.uk>
2019-04-08 11:04:13 +0100

    Set healthcheck script back to previous content
    The servertest env's SSL is dealt with by the load balancer so we don't
    need to check for it here

--- bin/check_health.sh
+++ bin/check_health.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [[ -z "${SECURE_USERNAME}" ]]; then
-  exec curl --insecure --fail https://localhost:3000/
+  exec curl --fail http://localhost:3000/
 else
-  exec curl --insecure --user $SECURE_USERNAME:$SECURE_PASSWORD --fail https://localhost:3000/
+  exec curl --user $SECURE_USERNAME:$SECURE_PASSWORD --fail http://localhost:3000/
 fi
