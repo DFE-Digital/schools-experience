ec0d1ff421d2b9a7991822f0d7d984b2b76b70c3
Peter Yates <peter.yates@graphia.co.uk>
2019-04-12 12:04:25 +0100

    Add specs for Schools::DashboardsHelper

--- app/helpers/schools/dashboards_helper.rb
+++ app/helpers/schools/dashboards_helper.rb
@@ -1,6 +1,6 @@
 module Schools::DashboardsHelper
-  def numbered_circle(number, colour: 'red', font_size: "16px", circle_size: 13)
-    content_tag(:svg, width: 26, height: 30) do
+  def numbered_circle(number, colour: 'red', width: 26, height: 30, font_size: "16px", circle_size: 13)
+    content_tag(:svg, width: width, height: height) do
       safe_join([
         tag(:circle, class: colour, cx: circle_size, cy: circle_size, r: circle_size),
         content_tag(:text, x: "50%", y: "50%", dy: "0.2em", "font-size" => font_size) do
