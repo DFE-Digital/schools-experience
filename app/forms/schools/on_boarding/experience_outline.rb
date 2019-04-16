1384ecb9a5a6a83d7c0386e9f655244fc2549b13
Richard Lynch <rjlynchdev@gmail.com>
2019-04-16 16:29:35 +0100

    remove todo

--- app/forms/schools/on_boarding/experience_outline.rb
+++ app/forms/schools/on_boarding/experience_outline.rb
@@ -12,7 +12,6 @@ module Schools
       validates :candidate_experience, presence: true
       validates :provides_teacher_training, inclusion: [true, false]
       validates :teacher_training_details, presence: true, if: :provides_teacher_training
-      # TODO validate url format
       validates :teacher_training_url, presence: true, if: :provides_teacher_training
       validates :teacher_training_url, format: URI::regexp(%w(http https)), if: -> { teacher_training_url.present? }
 
