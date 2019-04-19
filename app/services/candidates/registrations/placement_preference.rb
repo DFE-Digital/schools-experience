d4d32ea52a75824935456e70a1036043772ff7f4
Peter Yates <peter.yates@graphia.co.uk>
2019-04-19 18:23:21 +0100

    Add link between placement preference and date

--- app/services/candidates/registrations/placement_preference.rb
+++ app/services/candidates/registrations/placement_preference.rb
@@ -3,6 +3,7 @@ module Candidates
     class PlacementPreference < RegistrationStep
       include Behaviours::PlacementPreference
 
+      attribute :bookings_placement_date_id, :integer
       attribute :availability, :string
       attribute :objectives, :string
     end
