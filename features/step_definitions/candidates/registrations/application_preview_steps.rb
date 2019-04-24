db8e3ee57022093591d62cfce98e6c386b8471f9
Peter Yates <peter.yates@graphia.co.uk>
2019-04-24 15:25:56 +0100

    Add application preview feature and step defs

---
+++ features/step_definitions/candidates/registrations/application_preview_steps.rb
@@ -0,0 +1,110 @@
+Given("I have completed the wizard") do
+  visit path_for('request school experience placement', school: @school)
+  step "I have filled in my placement preferences successfully"
+  step "I have filled in my contact information successfully"
+  step "I have filled in my subject preferences successfully"
+  step "I have filled in my background checks successfully"
+end
+
+Given("I have filled in my placement preferences successfully") do
+  # Submit registrations/placement_preference form successfully
+  if @fixed_dates
+    choose @wanted_bookings_placement_date
+  else
+    fill_in 'Is there anything schools need to know about your availability for school experience?', with: 'Only free from Epiphany to Whitsunday'
+  end
+  fill_in 'What do you want to get out of your school experience?', with: 'I enjoy teaching'
+  click_button 'Continue'
+
+  expect(page.current_path).to eq \
+    "/candidates/schools/#{@school.urn}/registrations/contact_information/new"
+end
+
+Given("I have filled in my contact information successfully") do
+  # Submit contact information form successfully
+  fill_in 'Full name', with: 'testy mctest'
+  fill_in 'Email address', with: 'test@example.com'
+  fill_in 'Building', with: 'Test house'
+  fill_in 'Street', with: 'Test street'
+  fill_in 'Town or city', with: 'Test Town'
+  fill_in 'County', with: 'Testshire'
+  fill_in 'Postcode', with: 'TE57 1NG'
+  fill_in 'UK telephone number', with: '01234567890'
+  click_button 'Continue'
+  expect(page.current_path).to eq \
+    "/candidates/schools/#{@school.urn}/registrations/subject_preference/new"
+end
+
+Given("I have filled in my subject preferences successfully") do
+  # Submit registrations/subject_preference form successfully
+  choose 'Graduate or postgraduate'
+  select 'Physics', from: 'If you have or are studying for a degree, tell us about your degree subject'
+  choose "I’m very sure and think I’ll apply"
+  select 'Physics', from: 'First choice'
+  click_button 'Continue'
+  expect(page.current_path).to eq \
+    "/candidates/schools/#{@school.urn}/registrations/background_check/new"
+end
+
+Given("I have filled in my background checks successfully") do
+  # Submit registrations/background_check form successfully
+  choose 'Yes'
+  click_button 'Continue'
+  expect(page.current_path).to eq \
+    "/candidates/schools/#{@school.urn}/registrations/application_preview"
+end
+
+Given("my school has flexible dates") do
+  @fixed_dates = false
+  # do nothing, it's the default
+end
+
+Given("my school has fixed dates") do
+  @fixed_dates = true
+  @school_profile.update_attributes(availability_preference_fixed: true)
+  (1..3).each { |i| i.weeks.from_now }.each do |date|
+    @school_profile.bookings_placement_dates.create(date: date.weeks.from_now)
+    @wanted_bookings_placement_date = @school_profile.bookings_placement_dates.last.to_s
+  end
+  # do nothing, it's the default
+end
+
+Given("my school of choice exists") do
+  @school_profile = FactoryBot.create(:school_profile)
+  @school = @school_profile.bookings_school
+end
+
+Given("my school of choice offers {string}") do |string|
+  string.split(", ").each do |subject_name|
+    @school.subjects << FactoryBot.create(:bookings_subject, name: subject_name)
+  end
+end
+
+Given("My school offers physics") do
+  @school.subjects << FactoryBot.create(:bookings_subject, name: "Physics")
+end
+
+When("I am on the {string} page for my choice of school") do |string|
+  expect(page.current_path).to eql(path_for(string, school: @school.urn))
+end
+
+Then("I should see the following summary rows:") do |table|
+  table.hashes.each do |row|
+    within(".#{row['Heading'].tr(' ', '-').downcase}") do
+      expect(page).to have_css('dd', text: row['Value'])
+      expect(page).to have_link('Change', href: /#{row['Change link path']}/)
+    end
+  end
+end
+
+Then("I should see a summary row containing my selected date") do
+  within(".placement-date") do
+    expect(page).to have_content(@wanted_bookings_placement_date)
+  end
+end
+
+Then("the row should have a {string} link to {string}") do |link_text, path|
+  within(".placement-date") do
+    expect(page).to have_link(link_text, href: /#{path}/)
+  end
+end
