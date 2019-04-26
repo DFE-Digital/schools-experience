45bf65f0aeb40a4029951fa01b3b61bc3fdd677d
Richard Lynch <rjlynchdev@gmail.com>
2019-04-26 15:07:00 +0100

    use number_of_words validator

--- app/services/candidates/registrations/behaviours/placement_preference.rb
+++ app/services/candidates/registrations/behaviours/placement_preference.rb
@@ -4,9 +4,6 @@ module Candidates
       module PlacementPreference
         extend ActiveSupport::Concern
 
-        MAX_WORDS_FOR_AVAILABILITY = 149
-        MAX_WORDS_FOR_OBJECTIVE = 149
-
         included do
           validates :urn, presence: true
           validates :bookings_placement_date_id,
@@ -15,35 +12,15 @@ module Candidates
           validates :availability,
             presence: true,
             unless: -> { urn.present? && school_offers_fixed_dates? }
-          validate :availability_not_too_long, if: -> { availability.present? }
+          validates :availability, number_of_words: { less_than: 150 }, if: -> { availability.present? }
           validates :objectives, presence: true
-          validate  :objectives_not_too_long, if: -> { objectives.present? }
+          validates :objectives, number_of_words: { less_than: 150 }, if: -> { objectives.present? }
         end
 
       private
 
         def school_offers_fixed_dates?
           school.fixed_dates?
-        end
-
-        def availability_not_too_long
-          if availability_too_long?
-            errors.add :availability, :use_fewer_words
-          end
-        end
-
-        def availability_too_long?
-          availability.scan(/\s/).size > MAX_WORDS_FOR_AVAILABILITY
-        end
-
-        def objectives_not_too_long
-          if objectives_too_long?
-            errors.add :objectives, :use_fewer_words
-          end
-        end
-
-        def objectives_too_long?
-          objectives.scan(/\s/).size > MAX_WORDS_FOR_OBJECTIVE
         end
       end
     end
