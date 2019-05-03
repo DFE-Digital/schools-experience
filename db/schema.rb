77d9ac3d02edc4694c206f566a44c021c81279b1
Peter Yates <peter.yates@graphia.co.uk>
2019-05-03 13:22:44 +0100

    Add migration to rename Bookings::Phases
    Note we're referring to them by edubase_id because it wasn't
    automatically generated and set at import

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,8 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-ActiveRecord::Schema.define(version: 2019_05_02_084101) do
+
+ActiveRecord::Schema.define(version: 2019_05_03_120209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
