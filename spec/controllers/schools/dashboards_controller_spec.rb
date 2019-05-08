73382355bb6e2adda7b0c70494efdf0c0c233685
Peter Yates <peter.yates@graphia.co.uk>
2019-05-08 16:15:34 +0100

    Hide placement dates link for flexible schools

--- spec/controllers/schools/dashboards_controller_spec.rb
+++ spec/controllers/schools/dashboards_controller_spec.rb
@@ -53,5 +53,28 @@ describe Schools::DashboardsController, type: :request do
         expect(subject).to redirect_to(schools_errors_no_school_path)
       end
     end
+
+    context 'dashlette' do
+      include_context "logged in DfE user"
+      before { allow(Rails.application.config.x).to receive(:phase).and_return(2) }
+
+      context 'when the school has fixed dates' do
+        before { @current_user_school.update(availability_preference_fixed: true) }
+        before { get '/schools/dashboard' }
+
+        specify 'should have the link' do
+          expect(response.body).to match(/Update school experience dates/)
+        end
+      end
+
+      context 'when the school has flexible dates' do
+        before { @current_user_school.update(availability_preference_fixed: false) }
+        before { get '/schools/dashboard' }
+
+        specify 'should have the link' do
+          expect(response.body).not_to match(/Update school experience dates/)
+        end
+      end
+    end
   end
 end
