32dc102d3733520924f03671527dcb0012e9a9be
Richard Lynch <rjlynchdev@gmail.com>
2019-05-15 13:26:47 +0100

    Refactor feature specs
    Remove some duplicated steps from candidate registration features

--- features/candidates/registrations/wizard.feature
+++ features/candidates/registrations/wizard.feature
@@ -4,13 +4,12 @@ Feature: Persisting registration information on navigating away
   I want to be able to regain my place on a partially completed wizard
 
   Background:
-    Given I'm applying for a school
+    Given my school of choice exists
+    And my school of choice offers 'Physics'
+    And my school of choice offers 'Mathematics'
 
   Scenario: Returning to the wizard
-      Given I have completed the placement preference form
-      And I have completed the contact information form
-      And I have completed the subject preference form
-      And I have completed the background check form
+      Given I have completed the wizard
       And I have navigated away from the wizard
       When I come back to the wizard
       Then the placement preference form should populated with the details I've entered so far
--- features/step_definitions/candidates/registrations/wizard_steps.rb
+++ features/step_definitions/candidates/registrations/wizard_steps.rb
@@ -1,48 +1,3 @@
-Given("I'm applying for a school") do
-  @school = FactoryBot.create(:bookings_school)
-  @school.subjects.create! name: 'Physics'
-  @school.subjects.create! name: 'Mathematics'
-end
-
-Given("I have completed the placement preference form") do
-  visit path_for 'request school experience placement', school: @school
-  fill_in 'Is there anything schools need to know about your availability for school experience?', with: 'From Epiphany to Whitsunday'
-  fill_in 'What do you want to get out of your school experience?', with: 'I enjoy teaching'
-  click_button 'Continue'
-end
-
-Given("I have completed the contact information form") do
-  visit path_for 'enter your contact details', school: @school
-  fill_in 'Full name', with: 'testy mctest'
-  fill_in 'Day', with: '01'
-  fill_in 'Month', with: '01'
-  fill_in 'Year', with: '2000'
-  fill_in 'Email address', with: 'test@example.com'
-  fill_in 'Building', with: 'Test house'
-  fill_in 'Street', with: 'Test street'
-  fill_in 'Town or city', with: 'Test Town'
-  fill_in 'County', with: 'Testshire'
-  fill_in 'Postcode', with: 'TE57 1NG'
-  fill_in 'UK telephone number', with: '01234567890'
-  click_button 'Continue'
-end
-
-Given("I have completed the subject preference form") do
-  visit path_for 'candidate subjects', school: @school
-  choose 'Graduate or postgraduate'
-  select 'Physics', from: 'If you have or are studying for a degree, tell us about your degree subject'
-  choose 'I’ve applied for teacher training'
-  select 'Physics', from: 'First choice'
-  select 'Mathematics', from: 'Second choice'
-  click_button 'Continue'
-end
-
-Given("I have completed the background check form") do
-  visit path_for 'background checks', school: @school
-  choose 'Yes'
-  click_button 'Continue'
-end
-
 Given("I have navigated away from the wizard") do
   visit '/'
 end
@@ -55,7 +10,7 @@ Then("the placement preference form should populated with the details I've enter
   visit path_for 'request school experience placement', school: @school
   expect(find_field(
     'Is there anything schools need to know about your availability for school experience?'
-  ).value).to eq 'From Epiphany to Whitsunday'
+  ).value).to eq 'Only free from Epiphany to Whitsunday'
   expect(find_field('What do you want to get out of your school experience?').value).to eq 'I enjoy teaching'
 end
 
@@ -75,7 +30,7 @@ Then("the subject preference form should populated with the details I've entered
   visit path_for 'candidate subjects', school: @school
   expect(find_field('Graduate or postgraduate')).to be_checked
   expect(find_field('If you have or are studying for a degree, tell us about your degree subject').value).to eq 'Physics'
-  expect(find_field('I’ve applied for teacher training')).to be_checked
+  expect(find_field("I’m very sure and think I’ll apply")).to be_checked
   expect(find_field('First choice').value).to eq 'Physics'
   expect(find_field('Second choice').value).to eq 'Mathematics'
 end
