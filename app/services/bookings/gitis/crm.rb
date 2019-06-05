77199b0fda304402540df111651f74cda4a386fe
jebw <jeb@jdwilkins.co.uk>
2019-06-05 14:40:09 +0800

    Removed fake crm data from real Gitis::Crm object

--- app/services/bookings/gitis/crm.rb
+++ app/services/bookings/gitis/crm.rb
@@ -81,25 +81,6 @@ module Bookings
 
         parts.join(" #{join_with} ")
       end
-
-      def fake_account_data
-        {
-          'contactid' => "d778d663-a022-4c4b-9962-e469ee179f4a",
-          'firstname' => 'Matthew',
-          'lastname' => 'Richards',
-          'mobilephone' => '07123 456789',
-          'telephone1' => '01234 567890',
-          'emailaddress1' => 'first@thisaddress.com',
-          'emailaddress2' => 'second@thisaddress.com',
-          'emailaddress3' => 'third@thisaddress.com',
-          'address1_line1' => 'First Line',
-          'address1_line2' => 'Second Line',
-          'address1_line3' => 'Third Line',
-          'address1_city' => 'Manchester',
-          'address1_stateorprovince' => 'Manchester',
-          'address1_postalcode' => 'MA1 1AM'
-        }
-      end
     end
   end
 end
