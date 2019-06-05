4f5724d8cb0d5b76a6ae7f7be59a00b446573224
Peter Fry <peterfry@Peters-MacBook-Pro-2.local>
2019-06-05 13:47:39 +0100

    attempt dynamic generation of matrix

---
+++ script/generate_matrix.sh
@@ -0,0 +1,29 @@
+find . -type f -name "*.feature" > features.txt
+
+split -n l/5 features.txt
+
+ls -l
+
+index=1
+matrix="{"
+for browser in chrome firefox
+do
+  for filename in x*; do
+    data=$(
+    while read line
+    do
+      echo -n " ${line}"
+    done < $filename)
+    if [ $index -ne 1 ]
+    then
+      matrix+=","
+    fi
+    matrix+="'$browser_$index':{'browser':'$browser', 'features':'$data'}"
+    echo $index $data
+    ((index++))
+  done
+done
+matrix+="}"
+echo $matrix
+
+echo "##vso[task.setVariable variable=legs]${matrix}"
