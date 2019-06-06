020b95ce8e0de508aa21843bd2d0c5dd4786d542
jebw <jeb@jdwilkins.co.uk>
2019-06-06 11:49:55 +0100

    Added Candidate model

---
+++ app/models/bookings/candidate.rb
@@ -0,0 +1,6 @@
+class Bookings::Candidate < ApplicationRecord
+  UUID_V4_FORMAT = /\A[0-9a-f]{8}\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\b[0-9a-f]{12}\z/.freeze
+
+  validates :gitis_uuid, presence: true, format: { with: UUID_V4_FORMAT }
+  validates :gitis_uuid, uniqueness: { case_sensitive: false }
+end
