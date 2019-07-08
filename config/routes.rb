d8f05ddd96193c924071632ed77afec32cb2d95a
Peter Yates <peter.yates@graphia.co.uk>
2019-07-08 16:34:20 +0100

    Remove references to Phase 2

--- config/routes.rb
+++ config/routes.rb
@@ -9,76 +9,74 @@ Rails.application.routes.draw do
   get '/schools_privacy_policy', to: 'pages#schools_privacy_policy'
   get '/service_update', to: 'pages#service_update'
 
-  if Rails.application.config.x.phase >= 2
-    get '/auth/callback', to: 'schools/sessions#create'
+  get '/auth/callback', to: 'schools/sessions#create'
 
-    if Rails.env.servertest? || Rails.env.test?
-      get '/auth/insecure_callback', to: 'schools/insecure_sessions#create', as: :insecure_auth_callback
+  if Rails.env.servertest? || Rails.env.test?
+    get '/auth/insecure_callback', to: 'schools/insecure_sessions#create', as: :insecure_auth_callback
+  end
+
+  resource :schools, only: :show
+  namespace :schools do
+    resource :session, only: %i(show destroy)
+    resource :switch, only: %i(new), controller: 'switch'
+    resource :dashboard, only: :show
+    resource :toggle_enabled, only: %i(edit update), as: 'enabled', controller: 'toggle_enabled'
+
+    if Rails.application.config.x.phase >= 4
+      resources :placement_requests do
+        resource :cancellation, only: %i(show new create edit update), controller: 'placement_requests/cancellations' do
+          resource :notification_delivery, only: %i(show create), controller: 'placement_requests/cancellations/notification_deliveries'
+        end
+        namespace :acceptance do
+          resource :confirm_booking,
+            only: [:new, :create],
+            controller: '/schools/placement_requests/acceptance/confirm_booking'
+          resource :add_more_details,
+            only: [:new, :create],
+            controller: '/schools/placement_requests/acceptance/add_more_details'
+          resource :review_and_send_email,
+            only: [:new, :create],
+            controller: '/schools/placement_requests/acceptance/review_and_send_email'
+          resource :preview_confirmation_email,
+            only: [:new, :create],
+            controller: '/schools/placement_requests/acceptance/preview_confirmation_email'
+          resource :email_sent,
+            only: [:show],
+            controller: '/schools/placement_requests/acceptance/email_sent'
+        end
+      end
+      resources :confirmed_bookings, path: 'bookings', as: 'bookings', only: %i(index show) do
+        resource :cancellation, only: %i(show new create edit update), controller: 'confirmed_bookings/cancellations' do
+          resource :notification_delivery, only: %i(show create), controller: 'confirmed_bookings/cancellations/notification_deliveries'
+        end
+      end
     end
 
-    resource :schools, only: :show
-    namespace :schools do
-      resource :session, only: %i(show destroy)
-      resource :switch, only: %i(new), controller: 'switch'
-      resource :dashboard, only: :show
-      resource :toggle_enabled, only: %i(edit update), as: 'enabled', controller: 'toggle_enabled'
+    resource :availability_preference, only: %i(edit update)
+    resource :availability_info, only: %i(edit update), controller: 'availability_info'
+    resources :placement_dates
 
-      if Rails.application.config.x.phase >= 4
-        resources :placement_requests do
-          resource :cancellation, only: %i(show new create edit update), controller: 'placement_requests/cancellations' do
-            resource :notification_delivery, only: %i(show create), controller: 'placement_requests/cancellations/notification_deliveries'
-          end
-          namespace :acceptance do
-            resource :confirm_booking,
-              only: [:new, :create],
-              controller: '/schools/placement_requests/acceptance/confirm_booking'
-            resource :add_more_details,
-              only: [:new, :create],
-              controller: '/schools/placement_requests/acceptance/add_more_details'
-            resource :review_and_send_email,
-              only: [:new, :create],
-              controller: '/schools/placement_requests/acceptance/review_and_send_email'
-            resource :preview_confirmation_email,
-              only: [:new, :create],
-              controller: '/schools/placement_requests/acceptance/preview_confirmation_email'
-            resource :email_sent,
-              only: [:show],
-              controller: '/schools/placement_requests/acceptance/email_sent'
-          end
-        end
-        resources :confirmed_bookings, path: 'bookings', as: 'bookings', only: %i(index show) do
-          resource :cancellation, only: %i(show new create edit update), controller: 'confirmed_bookings/cancellations' do
-            resource :notification_delivery, only: %i(show create), controller: 'confirmed_bookings/cancellations/notification_deliveries'
-          end
-        end
-      end
+    namespace :errors do
+      resource :not_registered, controller: :not_registered, only: :show
+      resource :no_school, controller: :no_school, only: :show
+      resource :auth_failed, controller: :auth_failed, only: :show
+    end
 
-      resource :availability_preference, only: %i(edit update)
-      resource :availability_info, only: %i(edit update), controller: 'availability_info'
-      resources :placement_dates
-
-      namespace :errors do
-        resource :not_registered, controller: :not_registered, only: :show
-        resource :no_school, controller: :no_school, only: :show
-        resource :auth_failed, controller: :auth_failed, only: :show
-      end
-
-      namespace :on_boarding do
-        resource :candidate_requirement, only: %i(new create edit update)
-        resource :fees, only: %i(new create edit update)
-        resource :administration_fee, only: %i(new create)
-        resource :dbs_fee, only: %i(new create)
-        resource :other_fee, only: %i(new create)
-        resource :phases_list, only: %i(new create edit update)
-        resource :key_stage_list, only: %i(new create edit update)
-        resource :subjects, only: %i(new create edit update)
-        resource :description, only: %i(new create edit update)
-        resource :candidate_experience_detail, only: %i(new create edit update)
-        resource :experience_outline, only: %i(new create edit update)
-        resource :admin_contact, only: %i(new create edit update)
-        resource :profile, only: :show
-        resource :confirmation, only: %i(create show)
-      end
+    namespace :on_boarding do
+      resource :candidate_requirement, only: %i(new create edit update)
+      resource :fees, only: %i(new create edit update)
+      resource :administration_fee, only: %i(new create)
+      resource :dbs_fee, only: %i(new create)
+      resource :other_fee, only: %i(new create)
+      resource :phases_list, only: %i(new create edit update)
+      resource :key_stage_list, only: %i(new create edit update)
+      resource :subjects, only: %i(new create edit update)
+      resource :description, only: %i(new create edit update)
+      resource :candidate_experience_detail, only: %i(new create edit update)
+      resource :experience_outline, only: %i(new create edit update)
+      resource :admin_contact, only: %i(new create edit update)
+      resource :profile, only: :show
+      resource :confirmation, only: %i(create show)
     end
   end
 
