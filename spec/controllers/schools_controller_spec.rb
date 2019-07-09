2267c98ad4f813dc268e585f750830921e56223c
Peter Yates <peter.yates@graphia.co.uk>
2019-07-09 12:21:06 +0100

    No longer redirect to the dashboard when logged in
    Instead of automatically re-directing users to the dashboard from the
    landing page we want them to click 'Start now', which will bounce them
    to DfE Sign-in.
    
    This will hopefully make it more-obvious what's going on and help us
    diagnose some of the errors we're seeing in production

--- spec/controllers/schools_controller_spec.rb
+++ spec/controllers/schools_controller_spec.rb
@@ -3,15 +3,6 @@ require Rails.root.join("spec", "controllers", "schools", "session_context")
 
 describe SchoolsController, type: :request do
   describe 'redirecting logged-in users to the dashboard' do
-    context 'when a user is logged in' do
-      include_context "logged in DfE user"
-      before { get '/schools' }
-
-      specify 'should be redirected to the dashboard' do
-        expect(response).to redirect_to(schools_dashboard_path)
-      end
-    end
-
     context 'when no user is logged in' do
       before { get '/schools' }
 
