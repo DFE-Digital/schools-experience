e8fe43cda7d5dd5a38c466f42d248af9e628b795
Peter Yates <peter.yates@graphia.co.uk>
2019-07-12 13:50:22 +0100

    Only do one select to grab all relevant bookings

--- app/controllers/schools/confirm_attendance_controller.rb
+++ app/controllers/schools/confirm_attendance_controller.rb
@@ -3,33 +3,41 @@ module Schools
   # attended a booking
   class ConfirmAttendanceController < Schools::BaseController
     def show
-      @bookings = current_school
-        .bookings
-        .eager_load(:bookings_subject, bookings_placement_request: :candidate)
-        .previous
-        .attendance_unlogged
+      @bookings = unlogged_bookings.eager_load(
+        :bookings_subject,
+        bookings_placement_request: :candidate
+      )
 
       assign_gitis_contacts(@bookings)
     end
 
     def update
+      bookings_params = params
+        .select { |key,_| key.match(/\A\d+\z/) }
+        .transform_keys { |key| key.to_i }
+
+      bookings = unlogged_bookings.where(id: bookings_params.keys).index_by(&:id)
+
       Bookings::Booking.transaction do
-        params
-          .select { |k,_| k.match(/\A\d+\z/) }
-          .each do |booking_id, attended|
-            current_school
-              .bookings
-              .find(booking_id).tap do |booking|
-                booking.attended = ActiveModel::Type::Boolean.new.cast(attended)
-                booking.save
-              end
+        bookings_params.each do |booking_id, attended|
+          bookings[booking_id].tap do |booking|
+            booking.attended = ActiveModel::Type::Boolean.new.cast(attended)
+            booking.save
           end
+        end
 
         redirect_to schools_confirm_attendance_path
       end
     end
 
   private
+
+    def unlogged_bookings
+      current_school
+        .bookings
+        .previous
+        .attendance_unlogged
+    end
 
     def assign_gitis_contacts(bookings)
       return bookings if bookings.empty?
