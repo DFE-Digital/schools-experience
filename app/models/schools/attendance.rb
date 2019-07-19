a883fd5ae057ff783a1cac7045febb5424a25e80
Peter Yates <peter.yates@graphia.co.uk>
2019-07-19 10:23:05 +0100

    Add specs covering Bookings::Attendance behaviour

--- app/models/schools/attendance.rb
+++ app/models/schools/attendance.rb
@@ -6,17 +6,16 @@ module Schools
 
     def initialize(bookings:, bookings_params:)
       self.bookings = bookings
-
-      bookings_params.each do |booking_id, attended|
-        self.bookings.index_by(&:id).fetch(booking_id).tap do |booking|
-          booking.attended = ActiveModel::Type::Boolean.new.cast(attended)
-        end
-      end
+      self.bookings_params = bookings_params
     end
 
     def save
       Bookings::Booking.transaction do
-        self.bookings.each(&:save)
+        self.bookings_params.each do |booking_id, attended|
+          self.bookings.index_by(&:id).fetch(booking_id).tap do |booking|
+            booking.update(attended: ActiveModel::Type::Boolean.new.cast(attended))
+          end
+        end
       end
     end
   end
