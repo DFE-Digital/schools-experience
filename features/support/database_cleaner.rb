7d745e1f63d83efe85ff40821306d987d62729c3
Peter Fry <peterfry@peters-mbp-2.mynet>
2019-07-21 12:47:56 +0100

    trying with pre_count = true

--- features/support/database_cleaner.rb
+++ features/support/database_cleaner.rb
@@ -1,7 +1,9 @@
 exceptions = {except: %w{schema_migrations spatial_ref_sys}}
+  
+options = {:pre_count => true, except: %w{schema_migrations spatial_ref_sys}}
 
 DatabaseCleaner.clean_with(:truncation, exceptions)
-DatabaseCleaner.strategy = :truncation, exceptions
+DatabaseCleaner.strategy = :truncation, options
 Cucumber::Rails::Database.javascript_strategy = :truncation, exceptions
 if ENV['DEBUG_DATABASE_CLEANER'].present?
   ActiveRecord::Base.logger = Logger.new(STDOUT)
