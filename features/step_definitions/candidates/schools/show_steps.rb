8538384921193c6285dc3bf69645dd35130c7f80
Richard Lynch <rjlynchdev@gmail.com>
2019-07-24 16:17:07 +0100

    Fix cucumber complaining about checking for nil

--- features/step_definitions/candidates/schools/show_steps.rb
+++ features/step_definitions/candidates/schools/show_steps.rb
@@ -68,7 +68,7 @@ Then("I should see the entire school address in the sidebar") do
     @school.town,
     @school.county,
     @school.postcode
-  ].each do |address_part|
+  ].compact.each do |address_part|
     within(".govuk-grid-column-one-third") do
       within("#school-address") do
         expect(page).to have_content(address_part)
