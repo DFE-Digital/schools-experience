66e83f390c7a5e0e25a82ba95fa541106ebe4b6b
Peter Yates <peter.yates@graphia.co.uk>
2019-08-02 14:14:38 +0100

    Enhance features to cover autocomplete attributes

--- features/step_definitions/candidates/registrations/personal_information_steps.rb
+++ features/step_definitions/candidates/registrations/personal_information_steps.rb
@@ -4,3 +4,15 @@ Then("I should see a paragraph informing me that my details will be used to iden
       "Provide the following information so we can check if we already have your details."
   end
 end
+
+Then("the {string} date field should have day, month and year birth date auto-completion enabled") do |date_label|
+  date_field_set = page.find('.govuk-fieldset__heading', text: date_label)
+
+  within(date_field_set.ancestor('.govuk-form-group')) do
+    %w(Day Month Year).each do |part|
+      label = page.find('label', text: part)
+      input = page.find("input##{label[:for]}")
+      expect(input[:autocomplete]).to eql("bday bday-#{part.downcase}")
+    end
+  end
+end
