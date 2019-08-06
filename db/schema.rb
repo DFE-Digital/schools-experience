b8c84f4367280b8e396153eefe515c92058293de
Peter Yates <peter.yates@graphia.co.uk>
2019-08-06 11:47:07 +0100

    Add delayed_cron_job and run the generator

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_24_092556) do
+ActiveRecord::Schema.define(version: 2019_08_06_095819) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -247,6 +247,7 @@ ActiveRecord::Schema.define(version: 2019_07_24_092556) do
     t.string "queue"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string "cron"
     t.index ["priority", "run_at"], name: "delayed_jobs_priority"
   end
 
