b290273cef2c3b4fe6cf4335a80e7450afa3a73a
jebw <jeb@jdwilkins.co.uk>
2019-08-15 17:03:32 +0100

    Added initial version of Fake backend

--- app/services/bookings/gitis/fake_crm.rb
+++ app/services/bookings/gitis/fake_crm.rb
@@ -1,20 +1,25 @@
 module Bookings::Gitis
   module FakeCrm
-    KNOWN_UUID = "b8dd28e3-7bed-4cc2-9602-f6ee725344d2".freeze
-    REQUIRED = %w{
-      firstname lastname emailaddress2 telephone2 birthdate
-      address1_line1 address1_city address1_stateorprovince address1_postalcode
-      dfe_channelcreation dfe_hasdbscertificate
-      dfe_Country@odata.bind
-    }.freeze
-    ALLOWED = (
-      REQUIRED + %w{
-        telephone1 address1_telephone1 address1_line2 address1_line3
-        emailaddress1 dfe_dateofissueofdbscertificate
-        dfe_PreferredTeachingSubject01@odata.bind
-        dfe_PreferredTeachingSubject02@odata.bind
-      }
-    ).freeze
+    def initialize(*args)
+      super
+      @fake_backend = Backends::Fake.new
+    end
+
+    def fake_backend
+      @fake_backend
+    end
+
+    def fake_contact_id
+      fake_backend.send(:fake_contact_id)
+    end
+
+    def fake_contact_data
+      fake_backend.send(:fake_contact_data)
+    end
+
+    def backend
+      stubbed? ? @fake_backend : @backend
+    end
 
     def find_by_email(address)
       return super unless stubbed?
@@ -36,88 +41,8 @@ module Bookings::Gitis
 
   private
 
-    def create_entity(entity_id, data)
-      return super unless stubbed?
-      return "#{entity_id}(#{fake_contact_id})" unless entity_id == 'contacts'
-
-      REQUIRED.each do |key|
-        unless data.has_key?(key)
-          raise "Bad Response - attribute '#{key}' is missing"
-        end
-      end
-
-      data.keys.each do |key|
-        unless ALLOWED.include?(key)
-          raise "Bad Response - attribute '#{key}' is not recognised"
-        end
-      end
-
-      "#{entity_id}(#{fake_contact_id})"
-    end
-
-    def update_entity(entity_id, data)
-      return super unless stubbed?
-      return entity_id unless entity_id.start_with?('contacts(')
-
-      data.keys.each do |key|
-        unless ALLOWED.include?(key)
-          raise "Bad Response - attribute '#{key}' is not recognised"
-        end
-      end
-
-      entity_id
-    end
-
-    def fake_contact_id
-      fake_uuid = Rails.application.config.x.gitis.fake_crm_uuid
-
-      if %w{true yes 1}.include? fake_uuid
-        KNOWN_UUID
-      else
-        fake_uuid.presence || SecureRandom.uuid
-      end
-    end
-
-    def fake_contact_data
-      {
-        'contactid' => fake_contact_id,
-        'firstname' => 'Matthew',
-        'lastname' => 'Richards',
-        'mobilephone' => '07123 456789',
-        'telephone1' => '01234 567890',
-        'emailaddress1' => 'first@thisaddress.com',
-        'emailaddress2' => 'second@thisaddress.com',
-        'emailaddress3' => 'third@thisaddress.com',
-        'address1_line1' => 'First Line',
-        'address1_line2' => 'Second Line',
-        'address1_line3' => 'Third Line',
-        'address1_city' => 'Manchester',
-        'address1_stateorprovince' => 'Manchester',
-        'address1_postalcode' => 'TE57 1NG',
-        'address1_telephone1' => '01234 567890',
-        'birthdate' => '1980-01-01',
-        'dfe_channelcreation' => 10
-      }
-    end
-
     def stubbed?
       Rails.application.config.x.gitis.fake_crm
-    end
-
-    # only Contacts are mocked for now
-    def find_one(_entity_type, uuid, params)
-      return super unless stubbed?
-
-      Contact.new(fake_contact_data.merge('contactid' => uuid))
-    end
-
-    # only Contacts are mocked for now
-    def find_many(entity_type, uuids, params)
-      return super unless stubbed?
-
-      uuids.map do |uuid|
-        find_one(entity_type, uuid, params)
-      end
     end
   end
 end
