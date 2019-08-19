485399bf5e92892173a01ee721a7422ef6f73fed
Peter Yates <peter.yates@graphia.co.uk>
2019-08-19 15:03:49 +0100

    Allow syncing to be disabled by env var
    Setting the environment variable GIAS_SYNC_DISABLED to 'yes', 'true' or
    '1' will prevent the scheduled syncing from going ahead. Instead a
    warning will be printed to the log.

--- spec/models/bookings/school_sync_spec.rb
+++ spec/models/bookings/school_sync_spec.rb
@@ -6,4 +6,24 @@ describe Bookings::SchoolSync do
 
   specify { expect(subject.email_override).to eql(email_override) }
   specify { expect(subject).to respond_to(:sync) }
+
+  context 'when syncing is disabled' do
+    before do
+      allow(subject).to receive(:sync_disabled?).and_return(true)
+      allow(subject).to receive(:import_and_update).and_return(true)
+      allow(Rails.logger).to receive(:warn).and_return(true)
+    end
+
+    let(:disabled_message) { 'GIAS sync attempted but disabled' }
+
+    before { subject.sync }
+
+    specify 'calling sync should print a warning to the log' do
+      expect(Rails.logger).to have_received(:warn).with(disabled_message)
+    end
+
+    specify 'the sync should not go ahead' do
+      expect(subject).not_to have_received(:import_and_update)
+    end
+  end
 end
