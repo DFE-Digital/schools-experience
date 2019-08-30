025e4242e7b3f11cb0130cd7a7ddd295777d2e63
jebw <jeb@jdwilkins.co.uk>
2019-08-30 16:49:35 +0100

    Support switching devices as part of registration process
    A Candidate may start the Registration process on one device (eg their laptop),
    they will then expect to continue the process on the device they received their
    sign in link on, potentially their phone.
    
    We actually hold enough information to facilitate this - everything from the
    first step, Personal Information, is overridden by the GiTiS Contact
    information, and the school URN is included in the URN.
    
    1. This removes some unnecessary processing of the PersonalInformation now we
    instead override it with GitisRegistrationSession
    
    2. Removes off the step check for just the token sign in since thats not
    necessary in this context and it then allows the sign in to occur.

--- app/controllers/candidates/registrations/sign_ins_controller.rb
+++ app/controllers/candidates/registrations/sign_ins_controller.rb
@@ -1,6 +1,8 @@
 module Candidates
   module Registrations
     class SignInsController < RegistrationsController
+      skip_before_action :ensure_step_permitted!, only: :update
+
       def show
         @email_address = current_registration.personal_information.email
         @resend_link = candidates_school_registrations_sign_in_path
@@ -16,14 +18,11 @@ module Candidates
       end
 
       def update
-        personal_information = current_registration.personal_information
         candidate = Candidates::Session.signin!(params[:token])
 
         if candidate
-          personal_information.read_only = true
-          persist personal_information
-
           self.current_candidate = candidate
+
           redirect_to new_candidates_school_registrations_contact_information_path
         else
           @resend_link = candidates_school_registrations_sign_in_path
