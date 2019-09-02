cca70e51c4ed20a19d82684b9b937837c0df83df
jebw <jeb@jdwilkins.co.uk>
2019-09-02 11:14:20 +0100

    dont overwrite channel creation in factory

--- spec/factories/bookings/gitis/contact_factory.rb
+++ spec/factories/bookings/gitis/contact_factory.rb
@@ -11,7 +11,6 @@ FactoryBot.define do
     address1_stateorprovince { "Test County" }
     address1_postalcode { "MA1 1AM" }
     birthdate { '1980-01-01' }
-    dfe_channelcreation { 10 }
     dfe_hasdbscertificate { true }
     _dfe_preferredteachingsubject01_value { '04fc5f49-887b-4ac6-82ea-4278e9f3a9c1' }
     _dfe_preferredteachingsubject02_value { '686ba354-2a91-430b-affa-68b4e1fdffc0' }
