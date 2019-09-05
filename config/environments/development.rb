36bf460005db26948019238b7eb3d5bd95af6704
jebw <jeb@jdwilkins.co.uk>
2019-09-05 16:54:30 +0100

    Renamed environment variable

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -103,7 +103,7 @@ Rails.application.configure do
   config.x.gitis.service_url = ENV.fetch('CRM_SERVICE_URL', 'notset')
   config.x.gitis.channel_creation = ENV.fetch('CRM_CHANNEL_CREATION', '0')
   config.x.gitis.country_id = ENV.fetch('CRM_COUNTRY_ID', SecureRandom.uuid)
-  config.x.gitis.privacy_policy_id = ENV['CRM_PRIVACYPOLICY_ID'].presence
+  config.x.gitis.privacy_policy_id = ENV['CRM_PRIVACY_POLICY_ID'].presence
   if config.x.gitis.privacy_policy_id
     config.x.gitis.privacy_consent_id = ENV.fetch('CRM_PRIVACY_CONSENT_ID')
   end
