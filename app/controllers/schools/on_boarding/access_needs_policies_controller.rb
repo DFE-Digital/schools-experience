c775c1e233b0cda602b9161102bcd4d68f4194be
Richard Lynch <rjlynchdev@gmail.com>
2019-09-06 22:23:25 +0100

    Makes access_needs_policie editable

--- app/controllers/schools/on_boarding/access_needs_policies_controller.rb
+++ app/controllers/schools/on_boarding/access_needs_policies_controller.rb
@@ -18,6 +18,23 @@ module Schools
         end
       end
 
+      def edit
+        @access_needs_policy = current_school_profile.access_needs_policy
+      end
+
+      def update
+        @access_needs_policy = AccessNeedsPolicy.new access_needs_policy_params
+
+        if @access_needs_policy.valid?
+          current_school_profile.update! \
+            access_needs_policy: @access_needs_policy
+
+          redirect_to next_step_path(current_school_profile)
+        else
+          render :edit
+        end
+      end
+
     private
 
       def access_needs_policy_params
