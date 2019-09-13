4e26bcd5277a0e303565403c219e3fe816f15fa0
Peter Yates <peter.yates@graphia.co.uk>
2019-09-13 16:16:32 +0100

    Force column info reload before appyling update
    In the dev environment this migration failed because the schema hadn't
    been reloaded between the column being added and the following line
    attepting to write to it.
    
    Calling #reset_column_information between those two events should
    fix it

--- db/migrate/20190911085544_add_secondary_subject_flag_to_subjects.rb
+++ db/migrate/20190911085544_add_secondary_subject_flag_to_subjects.rb
@@ -2,6 +2,8 @@ class AddSecondarySubjectFlagToSubjects < ActiveRecord::Migration[5.2]
   def up
     add_column :bookings_subjects, :secondary_subject, :boolean, null: false, default: true
 
+    Bookings::Subject.reset_column_information
+
     Bookings::Subject.find_by(name: 'Primary').update(secondary_subject: false)
   end
 
