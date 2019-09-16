2becf41e2211f27f1b15d8a3c54ce071f20e1917
Peter Yates <peter.yates@graphia.co.uk>
2019-09-16 14:01:08 +0100

    Add a generic #govuk_link_to helper

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -102,4 +102,69 @@ describe ApplicationHelper, type: :helper do
       end
     end
   end
+
+  describe '#govuk_link_to' do
+    let(:path) { candidates_root_path }
+    let(:link_text) { 'Continue' }
+    subject { helper.govuk_link_to(link_text, path) }
+
+    specify 'should create a button link with the appropriate data module' do
+      expect(subject).to have_css("a.govuk-button[data-module='govuk-button']")
+    end
+
+    context 'overriding text' do
+      let(:custom_text) { 'Proceed' }
+      subject { helper.govuk_link_to(custom_text, path) }
+
+      specify 'button should contain the provided text' do
+        expect(subject).to have_css('a.govuk-button', text: custom_text)
+      end
+    end
+
+    context 'extra attributes' do
+      subject { helper.govuk_link_to(link_text, path, role: 'button', disabled: true, data: { type: 'XYZ' }) }
+
+      specify 'button should have the extra attributes' do
+        expect(subject).to have_css(
+          "a.govuk-button[role='button'][disabled='disabled'][data-type='XYZ'][data-module='govuk-button']",
+          text: link_text
+        )
+      end
+    end
+
+    context 'extra classes' do
+      let(:extra_classes) { %w(pink stripy) }
+      subject { helper.govuk_link_to(link_text, path, class: extra_classes) }
+
+      specify 'should create a button link with extra provided classes' do
+        expect(subject).to have_css("a.#{extra_classes.join('.')}")
+      end
+    end
+
+    context 'secondary' do
+      subject { helper.govuk_link_to(link_text, path, secondary: true) }
+
+      specify 'should create a button link with secondary class' do
+        expect(subject).to have_css('a.govuk-button.govuk-button--secondary')
+      end
+    end
+
+    context 'warning' do
+      subject { helper.govuk_link_to(link_text, path, warning: true) }
+
+      specify 'should create a button link with warning class' do
+        expect(subject).to have_css('a.govuk-button.govuk-button--warning')
+      end
+    end
+
+    context 'blocks' do
+      let(:custom_text) { 'Advance' }
+
+      subject { helper.govuk_link_to(path) { custom_text } }
+
+      specify 'button should contain the text provided by the block' do
+        expect(subject).to have_css('a.govuk-button', text: custom_text)
+      end
+    end
+  end
 end
