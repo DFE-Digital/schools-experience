6d4bdfa8284daae5c6cf58c36933e93e70c9d3b1
Peter Yates <peter.yates@graphia.co.uk>
2019-09-23 10:28:15 +0100

    Add some informative text to the error page

--- app/controllers/schools/errors/insufficient_privileges_controller.rb
+++ app/controllers/schools/errors/insufficient_privileges_controller.rb
@@ -1,5 +1,7 @@
 module Schools
   module Errors
+    # user can log in via DfE Sign-in but doesn't have the 'School
+    # Experience Administrator' role
     class InsufficientPrivilegesController < ApplicationController
       def show; end
     end
