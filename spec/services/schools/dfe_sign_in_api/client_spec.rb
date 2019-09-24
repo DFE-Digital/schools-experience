c220345a282d2a3f7d58807ede3894bc685745a3
Peter Yates <peter.yates@graphia.co.uk>
2019-09-24 12:13:32 +0100

    Add specs covering enabling the role check

--- spec/services/schools/dfe_sign_in_api/client_spec.rb
+++ spec/services/schools/dfe_sign_in_api/client_spec.rb
@@ -36,8 +36,48 @@ describe Schools::DFESignInAPI::Client do
   end
 
   describe '.role_check_enabled?' do
-    specify 'should be disabled when the client is disabled'
-    specify 'should require the dfe_sign_in_api_role_check_enabled to be true'
-    specify 'should require both environment variables to be present'
+    context 'when the client is disabled' do
+      before { allow(subject).to receive(:enabled?).and_return(false) }
+      before { allow(ENV).to receive(:fetch).and_return(true) }
+
+      specify 'should be disabled' do
+        expect(subject).not_to be_role_check_enabled
+      end
+    end
+
+    context 'when the client is enabled' do
+      subject { described_class }
+      before { allow(subject).to receive(:enabled?).and_return(true) }
+      before { allow(Rails.application.config.x).to receive(:dfe_sign_in_api_role_check_enabled).and_return(true) }
+
+
+      context 'when role check is disabled' do
+        context 'when the DfE Sign-in role and service environment variables are absent' do
+          before { allow(ENV).to receive(:fetch).and_return(nil) }
+
+          specify 'should be disabled' do
+            expect(subject).not_to be_role_check_enabled
+          end
+        end
+      end
+
+      context 'when role check is enabled' do
+        context 'when the DfE Sign-in role and service environment variables are absent' do
+          before { allow(ENV).to receive(:fetch).and_return(nil) }
+
+          specify 'should be disabled' do
+            expect(subject).not_to be_role_check_enabled
+          end
+        end
+
+        context 'when the DfE Sign-in role and service environment variables are present' do
+          before { allow(ENV).to receive(:fetch).and_return('yes') }
+
+          specify 'should be enabled' do
+            expect(subject).to be_role_check_enabled
+          end
+        end
+      end
+    end
   end
 end
