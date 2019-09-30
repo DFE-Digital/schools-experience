e3b725f136d2a3886b35c7d4b42b320c415e8e12
Peter Yates <peter.yates@graphia.co.uk>
2019-09-30 14:56:27 +0100

    Add supports_subjects to phases, placement dates
    This flag is intended to act as an identifer on whether or not to
    present school users with the option to associate subjects with
    placement dates.
    
    For phases that don't "support subjects", like Primary, no
    subject-specificity should be displayed.
    
    This option will be presented to the user when creating placement dates
    that categorise the date as having subjects (primary) or not (secondary,
    college) when schools offer multiple phases

---
+++ db/migrate/20190930142055_add_supports_subjects_flag_to_bookings_placement_dates.rb
@@ -0,0 +1,5 @@
+class AddSupportsSubjectsFlagToBookingsPlacementDates < ActiveRecord::Migration[5.2]
+  def change
+    add_column :bookings_placement_dates, :supports_subjects, :boolean
+  end
+end
