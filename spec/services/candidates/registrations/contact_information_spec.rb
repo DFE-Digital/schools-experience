7280c93566ab8673109aa9fb91129091aec3898a
Jeb Wilkins <jeb@jdwilkins.co.uk>
2019-10-03 14:15:44 +0100

    Revert "Add length validations matching the gitis CRM API SE-1503"

--- spec/services/candidates/registrations/contact_information_spec.rb
+++ spec/services/candidates/registrations/contact_information_spec.rb
@@ -13,89 +13,50 @@ describe Candidates::Registrations::ContactInformation, type: :model do
   end
 
   context 'validations' do
-    context 'building' do
-      it { is_expected.to validate_presence_of :building }
+    it { is_expected.to validate_presence_of :building }
+    it { is_expected.to validate_presence_of :postcode }
+    it { is_expected.to validate_presence_of :phone }
 
-      let(:too_long_msg) { 'Building must be 250 characters or fewer' }
-      it { is_expected.to validate_length_of(:building).is_at_most(250).with_message(too_long_msg) }
-    end
+    context 'phone is present' do
+      VALID_NUMBERS = ['01434 634996', '+441434634996', '01234567890'].freeze
+      INVALID_NUMBERS = ['7', 'q', '+4414346349'].freeze
+      BLANK_NUMBERS = ['', ' ', '   '].freeze
 
-    context 'street' do
-      let(:too_long_msg) { 'Street must be 250 characters or fewer' }
+      context 'valid numbers' do
+        VALID_NUMBERS.each do |number|
+          subject { described_class.new phone: number }
+          before { subject.validate }
 
-      it { is_expected.to validate_length_of(:street).is_at_most(250).with_message(too_long_msg) }
-    end
-
-    context 'town_or_city' do
-      let(:too_long_msg) { 'Town or city must be 80 characters or fewer' }
-
-      it { is_expected.to validate_length_of(:town_or_city).is_at_most(80).with_message(too_long_msg) }
-    end
-
-    context 'county' do
-      let(:too_long_msg) { 'County must be 50 characters or fewer' }
-
-      it { is_expected.to validate_length_of(:county).is_at_most(50).with_message(too_long_msg) }
-    end
-
-
-    context 'postcode' do
-      it { is_expected.to validate_presence_of :postcode }
-
-      let(:too_long_msg) { 'Postcode must be 20 characters or fewer' }
-      it { is_expected.to validate_length_of(:postcode).is_at_most(20).with_message(too_long_msg) }
-    end
-
-    context 'phone' do
-      it { is_expected.to validate_presence_of :phone }
-
-      let(:too_long_msg) { 'Phone number must be 50 digits or fewer' }
-      it { is_expected.to validate_length_of(:phone).is_at_most(50).with_message(too_long_msg) }
-
-      context 'phone is present' do
-        VALID_NUMBERS = ['01434 634996', '+441434634996', '01234567890'].freeze
-        INVALID_NUMBERS = ['7', 'q', '+4414346349'].freeze
-        BLANK_NUMBERS = ['', ' ', '   '].freeze
-
-        context 'valid numbers' do
-          VALID_NUMBERS.each do |number|
-            subject { described_class.new phone: number }
-            before { subject.validate }
-
-            it "permits #{number}" do
-              expect(subject.errors[:phone]).to be_empty
-            end
+          it "permits #{number}" do
+            expect(subject.errors[:phone]).to be_empty
           end
         end
+      end
 
-        context 'invalid numbers' do
-          INVALID_NUMBERS.each do |number|
-            subject { described_class.new phone: number }
-            before { subject.validate }
+      context 'invalid numbers' do
+        INVALID_NUMBERS.each do |number|
+          subject { described_class.new phone: number }
+          before { subject.validate }
 
-            it "doesn't permit #{number}" do
-              expect(subject.errors[:phone]).to eq ["Enter a valid telephone number"]
-            end
+          it "doesn't permit #{number}" do
+            expect(subject.errors[:phone]).to eq ["Enter a valid telephone number"]
           end
         end
+      end
 
-        context 'blank number' do
-          BLANK_NUMBERS.each do |number|
-            subject { described_class.new phone: number }
-            before { subject.validate }
+      context 'blank number' do
+        BLANK_NUMBERS.each do |number|
+          subject { described_class.new phone: number }
+          before { subject.validate }
 
-            it "doesn't permit #{number}" do
-              expect(subject.errors[:phone]).to eq ["Enter your telephone number"]
-            end
+          it "doesn't permit #{number}" do
+            expect(subject.errors[:phone]).to eq ["Enter your telephone number"]
           end
         end
       end
     end
 
     context 'postcode is present' do
-      let(:too_long_msg) { 'Postcode must be 20 characters or fewer' }
-      it { is_expected.to validate_length_of(:postcode).is_at_most(20).with_message(too_long_msg) }
-
       VALID_POSTCODES = [
         'DN55 1PT',
         'CR2 6XH',
