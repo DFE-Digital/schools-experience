d4d51ce32cb5b93defcf260c77e960df310351a5
jebw <jeb@jdwilkins.co.uk>
2019-10-18 14:17:07 +0100

    Added RejectedRequests#index

---
+++ app/controllers/schools/rejected_requests_controller.rb
@@ -0,0 +1,28 @@
+module Schools
+  class RejectedRequestsController < Schools::BaseController
+    def index
+      @requests = scope
+        .includes(:candidate, :school_cancellation, placement_date: :subjects)
+        .page(params[:page])
+        .per(50)
+
+      assign_gitis_contacts @requests
+    end
+
+  private
+
+    def scope
+      current_school.placement_requests.rejected
+    end
+
+    def assign_gitis_contacts(requests)
+      return requests if requests.empty?
+
+      contacts = gitis_crm.find(requests.map(&:contact_uuid)).index_by(&:id)
+
+      requests.each do |req|
+        req.candidate.gitis_contact = contacts[req.contact_uuid]
+      end
+    end
+  end
+end
