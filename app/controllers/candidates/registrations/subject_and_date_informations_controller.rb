94ea78d60c507cd22319a02156872d4b3cc27771
Peter Yates <peter.yates@graphia.co.uk>
2019-10-21 13:33:27 +0100

    Move date listing functionality to service object

--- app/controllers/candidates/registrations/subject_and_date_informations_controller.rb
+++ app/controllers/candidates/registrations/subject_and_date_informations_controller.rb
@@ -1,17 +1,7 @@
 module Candidates
   module Registrations
     class SubjectAndDateInformationsController < RegistrationsController
-      before_action :set_school, :set_primary_placement_dates, :set_secondary_placement_dates
-
-      PlacementDateOption = Struct.new(:placement_date_id, :placement_date_subject_id, :name, :duration) do
-        def id
-          [placement_date_id, placement_date_subject_id].compact.join('_')
-        end
-
-        def name_with_duration
-          "#{name} (#{duration} #{'day'.pluralize(duration)})"
-        end
-      end
+      before_action :set_school
 
       def new
         @subject_and_date_information = SubjectAndDateInformation.new(attributes_from_session)
@@ -52,46 +42,6 @@ module Candidates
         @school = current_registration.school
       end
 
-      def set_primary_placement_dates
-        @primary_placement_dates = @school
-          .bookings_placement_dates
-          .in_date_order
-          .not_supporting_subjects
-      end
-
-      def set_secondary_placement_dates
-        @secondary_placement_dates_grouped_by_date =  \
-          secondary_placement_dates.each_value { |v| v.sort_by!(&:name) }
-      end
-
-      def secondary_placement_dates
-        @school
-          .bookings_placement_dates
-          .in_date_order
-          .supporting_subjects
-          .eager_load(placement_date_subjects: :bookings_subject)
-          .published
-          .available
-          .each
-          .with_object({}) do |pd, h|
-            if h.has_key?(pd.date)
-              h[pd.date].concat(placement_date_options(pd))
-            else
-              h[pd.date] = Array.wrap(placement_date_options(pd))
-            end
-          end
-      end
-
-      def placement_date_options(placement_date)
-        if placement_date.placement_date_subjects.any?
-          placement_date.placement_date_subjects.map do |placement_date_subject|
-            PlacementDateOption.new(placement_date.id, placement_date_subject.id, placement_date_subject.bookings_subject.name, placement_date.duration)
-          end
-        else
-          Array.wrap(PlacementDateOption.new(placement_date.id, nil, 'All subjects', placement_date.duration))
-        end
-      end
-
       def subject_and_date_params
         params.require(:candidates_registrations_subject_and_date_information).permit(:subject_and_date_ids)
       end
@@ -107,7 +57,7 @@ module Candidates
       end
 
       def attributes_from_session
-        current_registration.subject_and_date_information_attributes
+        current_registration.subject_and_date_information_attributes.merge(urn: @school.urn)
       end
     end
   end
--- app/services/candidates/registrations/subject_and_date_information.rb
+++ app/services/candidates/registrations/subject_and_date_information.rb
@@ -3,6 +3,16 @@ module Candidates
     class SubjectAndDateInformation < RegistrationStep
       include Behaviours::SubjectAndDateInformation
 
+      PlacementDateOption = Struct.new(:placement_date_id, :placement_date_subject_id, :name, :duration) do
+        def id
+          [placement_date_id, placement_date_subject_id].compact.join('_')
+        end
+
+        def name_with_duration
+          "#{name} (#{duration} #{'day'.pluralize(duration)})"
+        end
+      end
+
       attribute :availability
       attribute :bookings_placement_date_id, :integer
       attribute :bookings_placement_dates_subject_id, :integer
@@ -19,6 +29,41 @@ module Candidates
 
       def subject_and_date_ids
         [bookings_placement_date_id, bookings_placement_dates_subject_id].join('_')
+      end
+
+      def primary_placement_dates
+        school
+          .bookings_placement_dates
+          .in_date_order
+          .not_supporting_subjects
+      end
+
+      def secondary_placement_dates_grouped_by_date
+        school
+          .bookings_placement_dates
+          .in_date_order
+          .supporting_subjects
+          .eager_load(placement_date_subjects: :bookings_subject)
+          .published
+          .available
+          .each
+          .with_object({}) do |pd, h|
+            if h.has_key?(pd.date)
+              h[pd.date].concat(placement_date_options(pd))
+            else
+              h[pd.date] = Array.wrap(placement_date_options(pd))
+            end
+          end
+      end
+
+      def placement_date_options(placement_date)
+        if placement_date.placement_date_subjects.any?
+          placement_date.placement_date_subjects.map do |placement_date_subject|
+            PlacementDateOption.new(placement_date.id, placement_date_subject.id, placement_date_subject.bookings_subject.name, placement_date.duration)
+          end
+        else
+          Array.wrap(PlacementDateOption.new(placement_date.id, nil, 'All subjects', placement_date.duration))
+        end
       end
     end
   end
