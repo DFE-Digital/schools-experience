8a5dea8199537aed877f903edd9058e15d20a402
jebw <jeb@jdwilkins.co.uk>
2019-10-31 12:47:31 +0000

    Added custom validator for format of email addresses

---
+++ app/validators/email_format_validator.rb
@@ -0,0 +1,21 @@
+class EmailFormatValidator < ActiveModel::EachValidator
+  EMAIL_WITH_FULLY_QUALIFIED_HOSTNAME = %r{\A[^\s@]+@[^\.\s]+\.[^\s]+\z}.freeze
+
+  def validate_each(record, attribute, value)
+    unless value.present? && is_an_email_uri?(value) && is_fqdn?(value)
+      record.errors.add(attribute, :invalid)
+    end
+  end
+
+private
+
+  # ensure it's a HTTP/HTTPS uri with at least one '.' in the hostname
+  # note that HTTPS inherits from HTTP
+  def is_an_email_uri?(value)
+    !!value.to_s.match?(URI::MailTo::EMAIL_REGEXP)
+  end
+
+  def is_fqdn?(value)
+    !!value.to_s.match?(EMAIL_WITH_FULLY_QUALIFIED_HOSTNAME)
+  end
+end
