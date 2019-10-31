377d69f0334906ae2447baeef412a71349bb7314
Richard Lynch <rjlynchdev@gmail.com>
2019-10-31 11:08:42 +0000

    Remove column default on supports_subjects
    This caused the supports subjects radio button to be pre selected. We
    want to leave this up to the school to decide.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_29_134926) do
+ActiveRecord::Schema.define(version: 2019_10_31_105854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,7 +76,7 @@ ActiveRecord::Schema.define(version: 2019_10_29_134926) do
     t.integer "max_bookings_count"
     t.datetime "published_at"
     t.boolean "subject_specific", default: false, null: false
-    t.boolean "supports_subjects", default: true, null: false
+    t.boolean "supports_subjects"
     t.index ["bookings_school_id"], name: "index_bookings_placement_dates_on_bookings_school_id"
   end
 
