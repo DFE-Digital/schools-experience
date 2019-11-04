c6e5cbb2534379d7e1e736bef9b21f34f02dc872
Richard Lynch <rjlynchdev@gmail.com>
2019-11-04 15:03:10 +0000

    Remove migration
    This migration updated the column default on a column the next migration
    removes. No need for this migration.

--- db/migrate/20191018212129_change_show_candidate_requirements_selection_column_default.rb
+++
@@ -1,5 +0,0 @@
-class ChangeShowCandidateRequirementsSelectionColumnDefault < ActiveRecord::Migration[5.2]
-  def change
-    change_column_default :schools_school_profiles, :show_candidate_requirements_selection, true
-  end
-end
