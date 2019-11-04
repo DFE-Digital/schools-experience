4d390959ccca0de9058fa1480cac571b55174d34
Richard Lynch <rjlynchdev@gmail.com>
2019-11-04 10:54:04 +0000

    Fix migration
    Add defaults when creating this column, remove migration that adds
    column defaults. The second migration errors when trying to add not null
    constraint as exiting rows have null columns.

--- db/migrate/20191008135000_add_subjects_supported_default_value.rb
+++
@@ -1,9 +0,0 @@
-class AddSubjectsSupportedDefaultValue < ActiveRecord::Migration[5.2]
-  def up
-    change_column :bookings_placement_dates, :supports_subjects, :boolean, null: false, default: true
-  end
-
-  def down
-    change_column :bookings_placement_dates, :supports_subjects, :boolean
-  end
-end
