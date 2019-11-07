42699b9e5ee25934a8cf046ea62cff6f22127740
jebw <jeb@jdwilkins.co.uk>
2019-11-07 15:27:01 +0000

    Added writing to cache on find, and renamed to WriteOnlyCache

---
+++ app/services/bookings/gitis/store/write_only_cache.rb
@@ -0,0 +1,45 @@
+module Bookings
+  module Gitis
+    module Store
+      class WriteOnlyCache
+        VERSION = 'v1'.freeze
+
+        attr_reader :store, :cache, :namespace, :ttl
+        delegate :fetch, to: :store
+
+        def initialize(store, cache, namespace: nil, ttl: 1.hour)
+          @store = store
+          @cache = cache
+          @namespace = namespace
+          @ttl = ttl
+        end
+
+        def cache_key_for_entity(entity)
+          [namespace, entity.cache_key, VERSION].compact.join('/')
+        end
+
+        def find(entity_type, uuids, **options)
+          store.find(entity_type, uuids, **options).tap do |entities|
+            if options.empty?
+              cache.write_multi entities_to_cache(entities), expires_in: ttl
+            end
+          end
+        end
+
+        def write(entity)
+          store.write(entity).tap do
+            cache.delete cache_key_for_entity entity
+          end
+        end
+
+      private
+
+        def entities_to_cache(entities)
+          Array.wrap(entities).each.with_object({}) do |entity, cache_set|
+            cache_set[cache_key_for_entity(entity)] = entity.to_cache
+          end
+        end
+      end
+    end
+  end
+end
