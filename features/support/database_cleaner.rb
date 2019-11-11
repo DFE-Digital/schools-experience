2207da2c6a5aa31dc247300df267becf78a0ee3e
jebw <jeb@jdwilkins.co.uk>
2019-11-11 16:10:57 +0000

    Switch to using deletion strategy

--- features/support/database_cleaner.rb
+++ features/support/database_cleaner.rb
@@ -1,8 +1,8 @@
 exceptions = { except: %w{schema_migrations spatial_ref_sys} }
 
-DatabaseCleaner.clean_with(:truncation, exceptions)
-DatabaseCleaner.strategy = :truncation, exceptions
-Cucumber::Rails::Database.javascript_strategy = :truncation, exceptions
+DatabaseCleaner.clean_with :truncation, exceptions
+DatabaseCleaner.strategy = :deletion, exceptions
+Cucumber::Rails::Database.javascript_strategy = :deletion, exceptions
 if ENV['DEBUG_DATABASE_CLEANER'].present?
   ActiveRecord::Base.logger = Logger.new(STDOUT)
 end
