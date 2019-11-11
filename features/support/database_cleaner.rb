9981f4dbbb3683788fdbbef6c46190c4fde393d0
jebw <jeb@jdwilkins.co.uk>
2019-11-11 16:40:33 +0000

    Working deletion strategy for Candidates screens

--- features/support/database_cleaner.rb
+++ features/support/database_cleaner.rb
@@ -1,10 +1,20 @@
 require 'database_cleaner/prevent_disabling_referential_integrity'
 
-exceptions = { except: %w{schema_migrations spatial_ref_sys} }
+truncation_options = { except: %w(schema_migrations spatial_ref_sys) }
+deletion_options = {
+  only: %w(
+    bookings_placement_request_cancellations
+    bookings_bookings bookings_placement_requests
+    candidates_session_tokens bookings_candidates
+    bookings_profiles bookings_placement_date_subjects bookings_placement_dates
+    bookings_schools_subjects bookings_schools_phases bookings_schools
+    bookings_subjects bookings_phases
+  )
+}
 
-DatabaseCleaner.clean_with :truncation, exceptions
-DatabaseCleaner.strategy = :deletion, exceptions
-Cucumber::Rails::Database.javascript_strategy = :deletion, exceptions
+DatabaseCleaner.clean_with :truncation, truncation_options
+DatabaseCleaner.strategy = :deletion, deletion_options
+Cucumber::Rails::Database.javascript_strategy = :deletion, deletion_options
 if ENV['DEBUG_DATABASE_CLEANER'].present?
   ActiveRecord::Base.logger = Logger.new(STDOUT)
 end
