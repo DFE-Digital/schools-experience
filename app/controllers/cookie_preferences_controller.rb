c712720c4e9882e58ff0c0aa945b8dcf4dc8315f
jebw <jeb@jdwilkins.co.uk>
2019-11-18 16:23:57 +0000

    Added preferences template

--- app/controllers/cookie_preferences_controller.rb
+++ app/controllers/cookie_preferences_controller.rb
@@ -1,5 +1,19 @@
 class CookiePreferencesController < ApplicationController
+  before_action :load_existing_preference, except: :show
+
   def show
     redirect_to edit_cookie_preference_path
+  end
+
+  def edit; end
+
+  def update
+    render 'edit'
+  end
+
+private
+
+  def load_existing_preference
+    @preference = CookiePreference.new
   end
 end
--- spec/controllers/cookie_preferences_controller_spec.rb
+++ spec/controllers/cookie_preferences_controller_spec.rb
@@ -1,8 +1,22 @@
 require 'rails_helper'
 
 describe CookiePreferencesController, type: :request do
+  subject { response }
+
   describe "#show" do
-    subject! { get cookie_preference_path }
-    it { expect(response).to redirect_to(edit_cookie_preference_path) }
+    before { get cookie_preference_path }
+    it { is_expected.to redirect_to(edit_cookie_preference_path) }
+  end
+
+  describe "#edit" do
+    before { get edit_cookie_preference_path }
+    it { expect(subject.body).to match 'Edit your cookie settings' }
+  end
+
+  describe "#update" do
+    context "with invalid" do
+      before { patch cookie_preference_path }
+      it { expect(subject.body).to match 'Edit your cookie settings' }
+    end
   end
 end
