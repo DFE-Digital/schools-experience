32ce8d9a34b299970a6aeba1f9d2d863d7ab1fbb
Richard Lynch <rjlynchdev@gmail.com>
2019-11-19 10:53:03 +0000

    use slice

--- app/controllers/schools/confirm_attendance_controller.rb
+++ app/controllers/schools/confirm_attendance_controller.rb
@@ -28,12 +28,10 @@ module Schools
       params
         .select { |key, _| key.match(/\A\d+\z/) }
         .transform_keys(&:to_i)
-        .select { |key, _|
-          # Avoid throwing key error if the user hits back button then
-          # resubmits the form causing the params to no longer match up with
-          # the unlogged_bookings.
-          unlogged_bookings.ids.include? key
-        }
+        .slice(*unlogged_bookings.ids)
+        # Avoid throwing key error if the user hits back button then
+        # resubmits the form causing the params to no longer match up with
+        # the unlogged_bookings.
     end
 
     def unlogged_bookings
