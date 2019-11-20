604c00a4ed0673740275b7e169409d2dfdbec4ab
Peter Yates <peter.yates@graphia.co.uk>
2019-11-20 13:51:05 +0000

    Add feature for 'Accept request' button behaviour

--- features/step_definitions/schools/placement_requests/accept_steps.rb
+++ features/step_definitions/schools/placement_requests/accept_steps.rb
@@ -16,3 +16,29 @@ Then("every row of the booking details list should have a {string} link") do |st
     end
   end
 end
+
+Given("there is a new placement request with a future date") do
+  @profile = FactoryBot.create(:bookings_profile, school: @school)
+  placement_date = FactoryBot.create(:bookings_placement_date, bookings_school: @school)
+  @placement_request = FactoryBot.create(:bookings_placement_request, school: @school, placement_date: placement_date)
+end
+
+Given("there is a new placement request with a past date") do
+  @profile = FactoryBot.create(:bookings_profile, school: @school)
+  placement_date = FactoryBot.create(:bookings_placement_date, :in_the_past, bookings_school: @school)
+  @placement_request = FactoryBot.create(:bookings_placement_request, school: @school, placement_date: placement_date)
+end
+
+
+When("I am on the {string} screen for that placement request") do |string|
+  path = path_for(string, placement_request: @placement_request)
+  visit(path)
+  expect(page.current_path).to eql(path)
+end
+
+Then("the {string} link should take me to the {string} page") do |link_text, screen|
+  expect(page).to have_link(
+    link_text,
+    href: path_for(screen, placement_request: @placement_request)
+  )
+end
