31f0a0af7fb6c7d6fdd1fc10784194c3d1f4a2e7
Peter Yates <peter.yates@graphia.co.uk>
2019-11-21 10:18:07 +0000

    Add features covering email sending functionality

--- features/step_definitions/schools/placement_requests/preview_confirmation_email_steps.rb
+++ features/step_definitions/schools/placement_requests/preview_confirmation_email_steps.rb
@@ -1,13 +1,3 @@
-Given("I have completed the 'review and send email' page") do
-  @candidate_instructions = 'Please come to the reception in the East Building'
-  steps %(
-    Given I have progressed to the 'review and send email' page for my chosen placement request
-    And I enter '#{@candidate_instructions}' into the 'Candidate instructions' field
-    When I submit the form
-    Then I should be on the 'preview confirmation email' page for my chosen placement request
-  )
-end
-
 When("I enter some candidate instructions") do
   @candidate_instructions = 'Please come to the reception in the East Building'
   step "I enter '#{@candidate_instructions}' into the 'Candidate instructions' text area"
@@ -19,11 +9,23 @@ Then("the extra details from the school section should contain the relevant info
   end
 end
 
-
-Given("I think the email looks good") do
-  # do nothing
+Then("the placement request should be accepted") do
+  expect(@placement_request.reload.booking.accepted_at).not_to be_nil
 end
 
-Then("the placement request should be accepted") do
-  expect(@placement_request.booking.accepted_at).not_to be_nil
+Given("I am have progressed to the {string} page for the placement request") do |string|
+  @booking = FactoryBot.create(:bookings_booking, bookings_placement_request: @placement_request)
+  path = path_for(string, placement_request: @placement_request)
+  visit(path)
+  expect(page.current_path).to eql(path)
+end
+
+Then("I should see an email preview that has the following sections:") do |table|
+  table.transpose.raw.flatten.each do |row|
+    expect(page).to have_css('h3', text: row)
+  end
+end
+
+Given("I enter nothing into the {string} text area") do |string|
+  # Do nothing
 end
