d987bb08985d07d4f4fb10327d66555bbac1d1af
jebw <jeb@jdwilkins.co.uk>
2020-01-07 13:03:50 +0000

    More cleaning up date handling
    Renamed :future scope to :bookable_date on PlacementDate model
    - it is only used as part of the :available scope, and that uses it to
      find the dates which can be booked so are available to the user.
    
    Removed :past scope from Placementdate
    - it was unused

--- app/models/bookings/placement_date.rb
+++ app/models/bookings/placement_date.rb
@@ -52,13 +52,12 @@ module Bookings
       validates :subjects, absence: true, unless: :subject_specific?
     end
 
-    scope :future, -> { where(arel_table[:date].gteq(Time.now)) }
-    scope :past, -> { where(arel_table[:date].lt(Time.now)) }
+    scope :bookable_date, -> { where(arel_table[:date].gteq(Time.now)) }
     scope :in_date_order, -> { order(date: 'asc') }
     scope :active, -> { where(active: true) }
     scope :inactive, -> { where(active: false) }
 
-    scope :available, -> { published.active.future.in_date_order }
+    scope :available, -> { published.active.bookable_date.in_date_order }
     scope :published, -> { where.not published_at: nil }
 
     # 'supporting subjects' are dates belonging to phases where
