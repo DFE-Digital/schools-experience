a9ccc95a06b6457f2222525611941a2013d0cb7c
jebw <jeb@jdwilkins.co.uk>
2020-02-04 11:05:01 +0000

    Changed default page delay in cucumber to be 0 seconds

--- features/support/feature_helper.rb
+++ features/support/feature_helper.rb
@@ -1,5 +1,5 @@
 def delay_page_load
   if ENV['CUC_DRIVER'].present? || ENV['CUC_PAGE_DELAY'].present?
-    sleep Integer(ENV.fetch('CUC_PAGE_DELAY') { 2 })
+    sleep Integer(ENV.fetch('CUC_PAGE_DELAY') { 0 })
   end
 end
