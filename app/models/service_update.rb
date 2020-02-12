5821d0304f4888196a1b2e6ef1d7ad20b5123212
jebw <jeb@jdwilkins.co.uk>
2020-02-12 13:36:28 +0000

    Added loading a Service Update from a yaml file

--- app/models/service_update.rb
+++ app/models/service_update.rb
@@ -2,8 +2,30 @@ class ServiceUpdate
   include ActiveModel::Model
   include ActiveModel::Attributes
 
+  DATA_PATH = Rails.root.join('data', model_name.collection).freeze
+
   attribute :date, :date
   attribute :title, :string
   attribute :summary, :string
   attribute :content, :string
+
+  class << self
+    def find(date)
+      new load_yaml(date).merge(date: date)
+    end
+
+  private
+
+    def load_yaml(date)
+      YAML.load_file update_path date
+    end
+
+    def update_path(date)
+      data_path.join "#{date}.yml"
+    end
+
+    def data_path
+      DATA_PATH
+    end
+  end
 end
--- spec/models/service_update_spec.rb
+++ spec/models/service_update_spec.rb
@@ -10,6 +10,13 @@ describe ServiceUpdate, type: :model do
     }
   end
 
+  describe 'data_path' do
+    it "should be assigned" do
+      expect(described_class.send(:data_path)).to eql \
+        Rails.root.join('data', 'service_updates')
+    end
+  end
+
   describe 'attributes' do
     subject { described_class.new attrs }
 
@@ -17,5 +24,20 @@ describe ServiceUpdate, type: :model do
     it { is_expected.to have_attributes title: attrs[:title] }
     it { is_expected.to have_attributes summary: attrs[:summary] }
     it { is_expected.to have_attributes content: attrs[:content] }
+  end
+
+  describe '.find' do
+    before do
+      allow(described_class).to receive(:data_path).and_return \
+        Rails.root.join('spec', 'sample_data', 'service_updates')
+    end
+
+    let(:test_date) { '20200202' }
+    subject { described_class.find test_date }
+
+    it { is_expected.to have_attributes date: Date.parse('2020-02-02') }
+    it { is_expected.to have_attributes title: 'Test Service Update' }
+    it { is_expected.to have_attributes summary: 'Now you can apply for School Experience' }
+    it { is_expected.to have_attributes content: /School Experience.\nYou can search/i }
   end
 end
