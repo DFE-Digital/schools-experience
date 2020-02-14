0922a5b686c3a513b898a661b9c74449a3bad43c
jebw <jeb@jdwilkins.co.uk>
2020-02-14 15:03:53 +0000

    Service updates index page

--- app/controllers/service_updates_controller.rb
+++ app/controllers/service_updates_controller.rb
@@ -1,5 +1,6 @@
 class ServiceUpdatesController < ApplicationController
   def index
-    @service_updates = ServiceUpdate.latest(10)
+    @recent_updates = ServiceUpdate.latest(6)
+    @latest = @recent_updates.shift
   end
 end
--- spec/controllers/service_updates_controller_spec.rb
+++ spec/controllers/service_updates_controller_spec.rb
@@ -4,12 +4,12 @@ describe ServiceUpdatesController, type: :request do
   let(:updates) { build_list :service_update, 3 }
 
   describe '#index' do
-    before { allow(ServiceUpdate).to receive(:latest).with(10) { updates } }
+    before { allow(ServiceUpdate).to receive(:latest).with(6) { updates } }
     before { get service_updates_path }
     subject { response }
 
     it { is_expected.to have_http_status(:success) }
-    it { is_expected.to render_template 'show' }
+    it { is_expected.to render_template 'index' }
     it { is_expected.to render_template '_service_update' }
   end
 end
