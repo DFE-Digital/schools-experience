d49a5e4b732652a4c0391cf93f6dd4cb11cddeb8
jebw <jeb@jdwilkins.co.uk>
2020-02-20 17:11:24 +0000

    Move DfE Sign-in role check to the ChangeSchool model

--- app/controllers/schools/change_schools_controller.rb
+++ app/controllers/schools/change_schools_controller.rb
@@ -16,15 +16,11 @@ module Schools
       @change_school = Schools::ChangeSchool.new \
         current_user, school_uuids, change_school_params
 
-      new_school = @change_school.retrieve_valid_school!
-
-      unless user_has_role_at_school?(current_user.sub, new_school.urn)
-        raise Schools::ChangeSchool::InaccessibleSchoolError
-      end
-
-      self.current_school = new_school
+      self.current_school = @change_school.retrieve_valid_school!
 
       redirect_to schools_dashboard_path
+    rescue ActiveModel::ValidationError
+      raise Schools::ChangeSchool::InaccessibleSchoolError
     end
 
   private
@@ -39,19 +35,6 @@ module Schools
 
     def access_denied
       redirect_to schools_errors_inaccessible_school_path
-    end
-
-    def user_has_role_at_school?(user_uuid, organisation_uuid)
-      return true unless Schools::DFESignInAPI::Client.role_check_enabled?
-
-      Schools::DFESignInAPI::Roles.new(user_uuid, organisation_uuid).has_school_experience_role?
-    rescue Faraday::ResourceNotFound
-      # if the role isn't found the API returns a 404 - this means that the user
-      # has insufficient privileges but this *isn't* really an error, so log it
-      # and return false
-      Rails.logger.warn("Role query yielded 404, uuid: #{user_uuid}, urn: #{urn}")
-
-      false
     end
   end
 end
--- app/forms/schools/change_school.rb
+++ app/forms/schools/change_school.rb
@@ -3,10 +3,13 @@ module Schools
     include ActiveModel::Model
     include ActiveModel::Attributes
 
-    attr_reader :current_user, :organisation_uuids
+    attr_reader :current_user, :uuids_to_urns
 
-    attribute :urn
+    attribute :urn, :integer
     validates :urn, presence: true
+    validate :validate_user_has_role_at_school, if: -> { urn.present? }
+
+    delegate :role_check_enabled?, to: Schools::DFESignInAPI::Client
 
     def self.allow_school_change_in_app?
       [
@@ -15,9 +18,9 @@ module Schools
       ].all?
     end
 
-    def initialize(current_user, organisation_uuids, attributes = {})
-      @current_user       = current_user
-      @organisation_uuids = organisation_uuids
+    def initialize(current_user, uuids_to_urns, attributes = {})
+      @current_user   = current_user
+      @uuids_to_urns  = uuids_to_urns
 
       super attributes
     end
@@ -36,12 +39,43 @@ module Schools
       Bookings::School.ordered_by_name.where(urn: organisation_urns)
     end
 
+    def uuid
+      urns_to_uuids[urn]
+    end
+
     class InaccessibleSchoolError < StandardError; end
 
   private
 
     def organisation_urns
-      organisation_uuids.values
+      uuids_to_urns.values
+    end
+
+    def user_has_role_at_school?
+      return true unless role_check_enabled?
+
+      role_checker.has_school_experience_role?
+    rescue Faraday::ResourceNotFound
+      # if the role isn't found the API returns a 404 - this means that the user
+      # has insufficient privileges but this *isn't* really an error, so log it
+      # and return false
+      Rails.logger.warn("Role query yielded 404, uuid: #{user_uuid}, urn: #{urn}")
+
+      false
+    end
+
+    def role_checker
+      Schools::DFESignInAPI::Roles.new current_user[:dfe_sign_in_user_uuid], uuid
+    end
+
+    def urns_to_uuids
+      @uuids_to_urns.invert
+    end
+
+    def validate_user_has_role_at_school
+      unless user_has_role_at_school?
+        errors.add :urn, :missing_dfe_signin_role
+      end
     end
   end
 end
