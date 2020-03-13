a45fcf812c367cc6d9e7f70a95d3c559ded44e9c
jebw <jeb@jdwilkins.co.uk>
2020-03-13 15:14:25 +0000

    Turned cache_classes back on to stop cucumber from complaining
    This was changed because the Rails 6 app:upgrade wanted to disable this
    (which seems odd). Not convinced of the change and cucumber wants it on so
    reverting.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -8,7 +8,7 @@ require Rails.root.join('spec', 'support', 'notify_fake_client')
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.cache_classes = false
+  config.cache_classes = true
 
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
