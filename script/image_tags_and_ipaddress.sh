cb034f23ec595506b75d5fcfa4de13d075e41648
jebw <jeb@jdwilkins.co.uk>
2020-03-18 17:04:33 +0000

    Removed script thats been moved to the devops repo

--- script/image_tags_and_ipaddress.sh
+++
@@ -1,9 +0,0 @@
-if [ $BUILD_SOURCEBRANCHNAME == 'master' ]; then
-  echo "##vso[task.setvariable variable=schoolExperienceImageTag]v${BUILD_BUILDID}"
-  echo "##vso[task.setvariable variable=schoolExperienceImageTagStable]v${BUILD_BUILDID}-stable"
-else
-  echo "##vso[task.setvariable variable=schoolExperienceImageTag]v${BUILD_BUILDID}-${BUILD_SOURCEBRANCHNAME}"
-  echo "##vso[task.setvariable variable=schoolExperienceImageTagStable]v${BUILD_BUILDID}-${BUILD_SOURCEBRANCHNAME}-stable"
-fi
-
-echo "##vso[task.setvariable variable=agentIpAddress]$(curl -s https://api.ipify.org)"
