ef7dfa59ae1dede2b73550f8dddfbb4ccbde95a9
matt <mattonfoot@gmail.com>
2020-05-23 22:08:41 +0100

    update content security policy

--- app/controllers/concerns/maps_content_security_policy.rb
+++ app/controllers/concerns/maps_content_security_policy.rb
@@ -5,11 +5,12 @@ module MapsContentSecurityPolicy
     content_security_policy(only: :show) do |policy| # Allow google maps
       policy.connect_src :self, 'https://www.google.com'
       policy.font_src :self, :data
-      policy.img_src :self, :data, 'https://www.google.com', "https://www.google-analytics.com"
+      policy.img_src :self, :data, "https://www.google.com", "https://maps.googleapis.com", "https://www.google-analytics.com"
       policy.style_src :self, "'unsafe-inline'", 'https://www.google.com'
       policy.script_src :self, :data, "'unsafe-inline'",
         "'unsafe-eval'",
-        'https://www.google.com',
+        "https://www.google.com",
+        "https://maps.googleapis.com",
         "https://www.googletagmanager.com",
         "https://www.google-analytics.com",
         "https://az416426.vo.msecnd.net" # needed for AppInsights
