50899a3e44e3f8eb55ee298288e4c102530015b2
matt <mattonfoot@gmail.com>
2020-06-04 12:30:34 +0100

    update google maps spec tests

--- spec/helpers/candidates/maps_helper_spec.rb
+++ spec/helpers/candidates/maps_helper_spec.rb
@@ -5,22 +5,23 @@ RSpec.describe Candidates::MapsHelper, type: :helper do
     allow(Rails.application.config.x).to receive(:google_maps_key) { '12345' }
     @latitude = "53.4782"
     @longitude = "-2.2299"
+    @zoom = 8
   end
 
   context '.static_map_url' do
     subject do
-      static_map_url(@latitude, @longitude, mapsize: [300, 200], zoom: 8)
+      static_map_url(@latitude, @longitude, mapsize: [300, 200], zoom: @zoom)
     end
 
     it('should return a correct Google Maps url') do
       url = "https://maps.googleapis.com/maps/api/staticmap"
-      params = "size=300x200&key=12345&markers=#{@latitude}%2C#{@longitude}"
+      params = "center=#{@latitude}%2C#{@longitude}&key=12345&markers=#{@latitude}%2C#{@longitude}&scale=2&size=300x200&zoom=#{@zoom}"
       expect(subject).to eq("#{url}?#{params}")
     end
   end
 
   context '.ajax_map' do
-    subject { ajax_map(@latitude, @longitude, mapsize: [300, 200], zoom: 8) }
+    subject { ajax_map(@latitude, @longitude, mapsize: [300, 200], zoom: @zoom) }
 
     it "should include wrapping div" do
       expect(subject).to match('class="embedded-map"')
@@ -37,11 +38,11 @@ RSpec.describe Candidates::MapsHelper, type: :helper do
   end
 
   context '.external_map_url' do
-    subject { external_map_url(latitude: @latitude, longitude: @longitude, name: 'test') }
+    subject { external_map_url(latitude: @latitude, longitude: @longitude, name: 'test', zoom: @zoom) }
 
     it "should add in latitude and longitude" do
       expect(subject).to \
-        eq("https://www.google.com/maps/@#{@latitude},#{@longitude},14z")
+        eq("https://www.google.com/maps/@#{@latitude},#{@longitude},#{@zoom}z")
     end
   end
 end
