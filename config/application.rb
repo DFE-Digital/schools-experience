ead0f1354b7f66225806f06a7f69b06ca8642f41
matt <mattonfoot@gmail.com>
2021-01-15 08:05:37 +0000

    Rubocop - missing space

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,6 @@ module SchoolExperience
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    #ActiveSupport::Deprecation.silenced = true
+    # ActiveSupport::Deprecation.silenced = true
   end
 end
