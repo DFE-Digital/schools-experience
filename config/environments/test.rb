67240a85d49bb62eecfb9c009e11d9302f6d52ed
matt <mattonfoot@gmail.com>
2021-01-22 13:39:25 +0000

    reorder test environment config to match order of production

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -8,8 +8,12 @@ require Rails.root.join('spec', 'support', 'notify_fake_client')
 # and recreated between test runs. Don't rely on the data there!
 
 Rails.application.configure do
+  # Verifies that versions and hashed value of the package contents in the project's package.json
+  config.webpacker.check_yarn_integrity = false
+
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Code is not reloaded between requests.
   config.cache_classes = true
 
   # Do not eager load code on boot. This avoids loading your whole application
@@ -17,31 +21,80 @@ Rails.application.configure do
   # preloads Rails for running tests, you may have to set it to true.
   config.eager_load = false
 
-  # Configure public file server for tests with Cache-Control for performance.
-  config.public_file_server.enabled = true
-  config.public_file_server.headers = {
-    'Cache-Control' => "public, max-age=#{1.hour.to_i}"
-  }
-
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Raise exceptions instead of rendering exception templates.
-  config.action_dispatch.show_exceptions = false
-
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
 
-  # avoid second pass through SASS since thats incompatible with GovUK Frontend
+  # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
+  # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
+  # config.require_master_key = true
+
+  # Configure public file server for tests with Cache-Control for performance.
+  config.public_file_server.enabled = true
+  config.public_file_server.headers = {
+    # 'X-Content-Type-Options' => 'nosniff',
+    'Cache-Control' => "public, max-age=#{1.hour.to_i}"
+  }
+
+  # Compress JS using a preprocessor.
+  config.assets.js_compressor = nil
+
+  # Compress CSS using a preprocessor.
+  # We're already using sass and that is set to compress at compile time
+  # so a second pass is not needed - the second pass was (maybe still is)
+  # causing problems with GovUK frontend use of variables
   config.assets.css_compressor = nil
 
+  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  # config.assets.compile = false
+
+  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
+  # config.asset_host = 'http://assets.example.com'
+
+  # Specifies the header that your server uses for sending files.
+  # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
+  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
+
+  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+  # config.force_ssl = true
+
+  # Include generic and useful information about system operation, but avoid logging too much
+  # information to avoid inadvertent exposure of personally identifiable information (PII).
+  # config.log_level = :info
+
+  # Prepend all log lines with the following tags.
+  # config.log_tags = [:request_id, ->(_) { "PID:#{Process.pid}" }]
+
+  # Use a different cache store in production.
+  # config.cache_store = :mem_cache_store
+
+  # Use a real queuing backend for Active Job (and separate queues per environment).
+  # config.active_job.queue_adapter     = :resque
+  # config.active_job.queue_name_prefix = "school_experience_test"
+
   # config.action_mailer.perform_caching = false
+
+  # Ignore bad email addresses and do not raise email delivery errors.
+  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  # config.action_mailer.raise_delivery_errors = false
 
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   # config.action_mailer.delivery_method = :test
+
+  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
+  # the I18n.default_locale when a translation cannot be found).
+  # config.i18n.fallbacks = true
+
+  # Raises error for missing translations.
+  # config.i18n.raise_on_missing_translations = true
+
+  # Annotate rendered view with file names.
+  # config.action_view.annotate_rendered_view_with_filenames = true
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
@@ -51,6 +104,43 @@ Rails.application.configure do
 
   # Tell Active Support which deprecation messages to disallow.
   config.active_support.disallowed_deprecation_warnings = []
+
+  # Use default logging formatter so that PID and timestamp are not suppressed.
+  # config.log_formatter = ::Logger::Formatter.new
+
+  # Use a different logger for distributed setups.
+  # require "syslog/logger"
+  # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+
+  # if ENV["RAILS_LOG_TO_STDOUT"].present?
+  #  logger           = ActiveSupport::Logger.new(STDOUT)
+  #  logger.formatter = config.log_formatter
+  #  config.logger    = ActiveSupport::TaggedLogging.new(logger)
+  # end
+
+  # Do not dump schema after migrations.
+  # config.active_record.dump_schema_after_migration = false
+
+  # Inserts middleware to perform automatic connection switching.
+  # The `database_selector` hash is used to pass options to the DatabaseSelector
+  # middleware. The `delay` is used to determine how long to wait after a write
+  # to send a subsequent read to the primary.
+  #
+  # The `database_resolver` class is used by the middleware to determine which
+  # database is appropriate to use based on the time delay.
+  #
+  # The `database_resolver_context` class is used by the middleware to set
+  # timestamps for the last write to the primary. The resolver uses the context
+  # class timestamps to determine how long to wait before reading from the
+  # replica.
+  #
+  # By default Rails will store a last write timestamp in the session. The
+  # DatabaseSelector middleware is designed as such you can define your own
+  # strategy for connection switching and pass that into the middleware through
+  # these configuration options.
+  # config.active_record.database_selector = { delay: 2.seconds }
+  # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
+  # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   # Use Redis for Session and cache
   config.cache_store = :redis_cache_store,
@@ -64,29 +154,22 @@ Rails.application.configure do
     key: 'schoolex-test-session',
     expire_after: 1.hour # Sets explicit TTL for Session Redis keys
 
-  # Raises error for missing translations.
-  # config.i18n.raise_on_missing_translations = true
-
-  # Annotate rendered view with file names.
-  # config.action_view.annotate_rendered_view_with_filenames = true
+  # Raise exceptions instead of rendering exception templates.
+  config.action_dispatch.show_exceptions = false
 
   # Use the test adapter for active jobs
   config.active_job.queue_adapter = :test
 
-  config.after_initialize do
-    Bullet.enable = true
-    Bullet.raise = true
-  end
+  # config.force_ssl = true
+
+  Rails.application.routes.default_url_options = { protocol: 'https' }
 
   # Don't actually attempt to delivery emails during tests
   config.x.notify_client = NotifyFakeClient
 
+  config.x.default_phase = 4
   config.x.phase = 10_000
-  config.x.features = %i[
-    subject_specific_dates
-    capped_bookings
-    reminders
-  ]
+  # config.x.candidates.deactivate_applications = false
   config.x.candidates.disable_applications = false
   config.x.candidates.alert_notification = nil
   config.x.google_maps_key = nil
@@ -110,8 +193,20 @@ Rails.application.configure do
   config.x.gitis.privacy_consent_id = '10'
   config.x.gitis.caching = false
 
-  Rails.application.routes.default_url_options = { protocol: 'https' }
+  config.x.features = %i[
+    subject_specific_dates
+    capped_bookings
+    reminders
+  ]
+
+  config.sass[:style] = :compressed if config.sass
+
   config.ab_threshold = Integer ENV.fetch('AB_TEST_THRESHOLD', 100)
 
   config.x.maintenance_mode = false
+
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.raise = true
+  end
 end
