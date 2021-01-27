0e766331355b2f30b48d6919fb6fdba7b41505ad
jebw <jeb@jdwilkins.co.uk>
2021-01-27 17:38:24 +0000

    More debug info for missing URNs

--- app/controllers/schools/base_controller.rb
+++ app/controllers/schools/base_controller.rb
@@ -20,7 +20,10 @@ module Schools
     rescue_from Bookings::Gitis::API::ConnectionFailed, with: :gitis_retrieval_error
 
     def current_school
-      raise MissingURN, 'urn is missing, unable to match with school' if current_urn.blank?
+      if current_urn.blank?
+        sub = current_user.raw_attributes.dig("sub")
+        raise MissingURN, "school urn is missing, unable to match with school for - user #{sub}"
+      end
 
       @current_school ||= retrieve_school(current_urn)
     end
@@ -38,7 +41,10 @@ module Schools
     end
 
     def retrieve_school(urn)
-      raise MissingURN if urn.blank?
+      if urn.blank?
+        sub = current_user.raw_attributes.dig("sub")
+        raise MissingURN, "school urn is missing, unable to match with school for - user #{sub}"
+      end
 
       Bookings::School.find_by!(urn: urn)
     rescue ActiveRecord::RecordNotFound
