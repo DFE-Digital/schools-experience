5ae47781978937ed15b35ece46a4de21934c6ecf
jebw <jeb@jdwilkins.co.uk>
2021-01-30 10:57:01 +0000

    Added 'Yes - when in school' to DBS Requirements options

--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_27_200844) do
+ActiveRecord::Schema.define(version: 2021_01_30_095443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -174,6 +174,7 @@ ActiveRecord::Schema.define(version: 2021_01_27_200844) do
     t.boolean "disability_confident"
     t.boolean "has_access_needs_policy"
     t.string "access_needs_policy_url"
+    t.string "dbs_policy_conditions"
     t.index ["school_id"], name: "index_bookings_profiles_on_school_id", unique: true
   end
 
@@ -399,6 +400,8 @@ ActiveRecord::Schema.define(version: 2021_01_27_200844) do
     t.string "access_needs_policy_url"
     t.boolean "candidate_requirements_selection_provide_photo_identification"
     t.text "candidate_requirements_selection_photo_identification_details"
+    t.string "dbs_requirement_dbs_policy_conditions"
+    t.text "dbs_requirement_dbs_policy_details_inschool"
     t.index ["bookings_school_id"], name: "index_schools_school_profiles_on_bookings_school_id"
   end
 
