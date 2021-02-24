8d7b3e4248ae982df6129677b33b43edd8bb5a36
jebw <jeb@jdwilkins.co.uk>
2021-02-24 15:17:54 +0000

    Added URLs for linking to DSI service management

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -3,4 +3,7 @@ require File.expand_path("production.rb", __dir__)
 
 Rails.application.configure do
   # Override any production defaults here
+
+  config.x.dfe_sign_in_request_organisation_url = "https://pp-services.signin.education.gov.uk/request-organisation/search"
+  config.x.dfe_sign_in_request_service_url = "https://pp-services.signin.education.gov.uk/approvals/select-organisation?services=add"
 end
