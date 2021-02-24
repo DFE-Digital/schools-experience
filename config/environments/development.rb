97d8f41d2978c36e1fd09f09b7363ef5b4529ec7
jebw <jeb@jdwilkins.co.uk>
2021-02-24 17:08:43 +0000

    Link to 'Request access' page in DfE Sign-in from insufficient privs page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -117,7 +117,6 @@ Rails.application.configure do
   config.x.dfe_sign_in_api_role_check_enabled = ENV['DFE_SIGNIN_API_ROLE_CHECK_ENABLED']&.in?(truthy_strings)
   config.x.dfe_sign_in_api_school_change_enabled = ENV['DFE_SIGNIN_API_SCHOOL_CHANGE_ENABLED']&.in?(truthy_strings)
   config.x.dfe_sign_in_request_organisation_url = "https://pp-services.signin.education.gov.uk/request-organisation/search"
-  config.x.dfe_sign_in_request_service_url = "https://pp-services.signin.education.gov.uk/approvals/select-organisation?services=add"
 
   if ENV['NOTIFY_CLIENT'].present?
     Rails.application.config.x.notify_client = ENV['NOTIFY_CLIENT']
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -5,5 +5,4 @@ Rails.application.configure do
   # Override any production defaults here
 
   config.x.dfe_sign_in_request_organisation_url = "https://pp-services.signin.education.gov.uk/request-organisation/search"
-  config.x.dfe_sign_in_request_service_url = "https://pp-services.signin.education.gov.uk/approvals/select-organisation?services=add"
 end
