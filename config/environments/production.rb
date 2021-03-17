7900006375db24cac574999857612cffecd30822
jebw <jeb@jdwilkins.co.uk>
2021-03-17 17:06:13 +0000

    Corrected the naming of the keyword arguments for the redis error handler
    And excluded the proc from the unusued keywords lint since we require our
    current usage

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -128,7 +128,7 @@ Rails.application.configure do
                          url: ENV['REDIS_CACHE_URL'].presence || ENV['REDIS_URL'].presence,
                          reconnect_attempts: 1,
                          tcp_keepalive: 60,
-                         error_handler: lambda do |_method:, returning:, exception:|
+                         error_handler: lambda do |method:, returning:, exception:|
                                           Sentry.capture_exception(exception)
                                         end
                        }
