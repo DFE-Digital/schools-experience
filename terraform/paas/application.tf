6d80f5902dc039fa38dae97de3a38a93f3c2bb85
Steven Fawcett <steve.fawcett@capgemini.com>
2021-06-15 11:13:21 +0100

    Fix Database Connection
    This is for delivery to PaaS only.
    Remove the service binds and get the database name from the
    service-key

--- terraform/paas/application.tf
+++ terraform/paas/application.tf
@@ -1,11 +1,12 @@
 locals {
   environment_map = { REDIS_URL = local.redis-credentials.uri,
-    DATABASE_URL   = local.postgres-credentials.uri,
-    DB_DATABASE    = "schools-experience",
-    DB_HOST        = local.postgres-credentials.host,
-    DB_USERNAME    = local.postgres-credentials.username,
-    DB_PASSWORD    = local.postgres-credentials.password,
-    SKIP_FORCE_SSL = true,
+    DB_DATABASE         = local.postgres-credentials.name 
+    DB_HOST             = local.postgres-credentials.host,
+    DB_USERNAME         = local.postgres-credentials.username,
+    DB_PASSWORD         = local.postgres-credentials.password,
+    SKIP_FORCE_SSL      = true,
+    SENTRY_CURRENT_ENV  = var.application_environment,
+    SLACK_ENV           = var.application_environment
   }
 }
 
@@ -25,22 +26,6 @@ resource "cloudfoundry_app" "application" {
 
   routes {
     route = cloudfoundry_route.route_internal.id
-  }
-
-  # For Review Apps find existing Resources
-  dynamic "service_binding" {
-    for_each = concat(data.cloudfoundry_service_instance.redis, data.cloudfoundry_service_instance.redis)
-    content {
-      service_instance = service_binding.value["id"]
-    }
-  }
-
-  # For Dev/Test/Production Apps use created Resources
-  dynamic "service_binding" {
-    for_each = concat(cloudfoundry_service_instance.redis, cloudfoundry_service_instance.postgres, cloudfoundry_user_provided_service.logging)
-    content {
-      service_instance = service_binding.value["id"]
-    }
   }
 
   environment = merge(local.application_secrets, local.environment_map)
