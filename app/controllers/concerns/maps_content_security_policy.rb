ba6b18d6d21cab7090d5ec5f7049280f38c64737
Leonidas Apostolidis <leoapost@gmail.com>
2021-08-05 12:40:22 +0100

    Allow google fonts to be loaded

--- app/controllers/concerns/maps_content_security_policy.rb
+++ app/controllers/concerns/maps_content_security_policy.rb
@@ -4,7 +4,7 @@ module MapsContentSecurityPolicy
   included do
     content_security_policy(only: :show) do |policy| # Allow google maps
       policy.connect_src :self, 'https://www.google.com', 'https://www.google-analytics.com'
-      policy.font_src :self, :data, 'https://fonts.googleapis.com/'
+      policy.font_src :self, :data, "https://fonts.gstatic.com"
       policy.img_src :self, :data, "https://www.google.com", "https://maps.googleapis.com", "https://maps.gstatic.com/", "https://www.google-analytics.com"
       policy.style_src :self, "'unsafe-inline'", 'https://www.google.com', 'https://fonts.googleapis.com/'
       policy.script_src :self, :data, "'unsafe-inline'",
