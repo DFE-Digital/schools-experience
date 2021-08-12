2f0789be40ebc7332088b2224164ca42e155ff25
Leonidas Apostolidis <leoapost@gmail.com>
2021-08-12 14:38:35 +0100

    Update the form label for the maximum_distance_from_school field
    Make it clearer that the field expects a number value. Some users are
    confused from the example we provide and try to type the word "miles".

--- features/step_definitions/schools/onboarding_steps.rb
+++ features/step_definitions/schools/onboarding_steps.rb
@@ -42,7 +42,7 @@ Given "I have completed the Candidate Requirements selection step" do
     Given I am on the 'candidate requirements selection' page
     And I check "They must apply or have applied to your or a partner school's teacher training course"
     And I check "They must live locally"
-    And I enter '7' into the 'Tell us within how many miles of your school. For example, 20 miles.' text area
+    And I enter '7' into the 'Tell us within how many miles of your school. For example, 20.' text area
     And I check 'Other'
     And I enter 'Some details' into the 'Provide details.' text area
     When I submit the form
