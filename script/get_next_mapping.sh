2878f5beb3665525f8a5341fa11b1aff014ceae7
Steven Fawcett <steve.fawcett@capgemini.com>
2021-10-06 11:00:32 +0100

    Shell Script had terrbile indentations and few comments

--- script/get_next_mapping.sh
+++ script/get_next_mapping.sh
@@ -19,48 +19,50 @@ APPS=$(curl -s "${URL}/apps?per_page=2000"  -X GET  -H "Authorization: ${TOKEN}"
 # Check the route we want to add exists
 CURRENT=$(echo ${APPS} | jq --arg ROUTE "${ADD_ROUTE}"  '. | select (.name==$ROUTE) | {name,guid} ')
 if [[ -z "${CURRENT}"  ]] ; then
-   create=1
+    create=1
 else
-   create=1
-   GUID=$(echo ${CURRENT}  | jq -r '.guid' ) 
-   LIST_OF_ROUTES=$(curl -s "${URL}/routes?app_guids=${GUID}"  -X GET  -H "Authorization: ${TOKEN}" | jq '.resources[] | {host,guid,url}')
-   while read i; do
-      for (( v=${STATIC_START} ; v<${STATIC_END}+1 ; v++ )) 
-      do 
-          if [[ "${i}" == "${STATIC}${v}" ]] ; then
-	      echo "${i}"
-              create=0
-	      break
-          fi
-      done
+    create=1
+    GUID=$(echo ${CURRENT}  | jq -r '.guid' )
+    LIST_OF_ROUTES=$(curl -s "${URL}/routes?app_guids=${GUID}"  -X GET  -H "Authorization: ${TOKEN}" | jq '.resources[] | {host,guid,url}')
+    while read i; do
+        for (( v=${STATIC_START} ; v<${STATIC_END}+1 ; v++ ))
+        do
+            if [[ "${i}" == "${STATIC}${v}" ]] ; then
+                echo "${i}"
+                create=0
+                break
+            fi
+        done
     done <<< "$( echo "${LIST_OF_ROUTES}" | jq -r  '.host ' )"
 fi
 
 if [[ ${create} == 1 ]]; then
 
-	   CSV_LIST=$( echo "${APPS}" | jq -r -c  '.guid ' | tr '\n' ',' )
+    # Get a comman seperated list of Application GUIDs
+    CSV_LIST=$( echo "${APPS}" | jq -r -c  '.guid ' | tr '\n' ',' )
 
-           LIST_OF_ROUTES=$(curl -s "${URL}/routes?per_page=2000&app_guids=${CSV_LIST}"  -X GET  -H "Authorization: ${TOKEN}" | jq '.resources[] | {host,guid,url}')
+    # Get a list of all the routes for all the applications
+    LIST_OF_ROUTES=$(curl -s "${URL}/routes?per_page=2000&app_guids=${CSV_LIST}"  -X GET  -H "Authorization: ${TOKEN}" | jq '.resources[] | {host,guid,url}')
 
-	   for (( v=${STATIC_START} ; v<${STATIC_END}+1 ; v++ )) 
-           do 
-              USED[$v]=0
-              while read i; do
-		if [[ "${STATIC}${v}.${DOMAIN}" == ${i} ]]; then
-                    USED[$v]=1
-		fi
-	      done <<< "$(echo "${LIST_OF_ROUTES}" | jq -r -c  '.url ')"
-           done
+    # Run through each static route checking to see if it has been used. Store the result in an array
+    for (( v=${STATIC_START} ; v<${STATIC_END}+1 ; v++ ))
+    do
+        USED[$v]=0
+        # run through each application route and check if it has a static route
+        while read i; do
+            if [[ "${STATIC}${v}.${DOMAIN}" == ${i} ]]; then
+                USED[$v]=1
+            fi
+        done <<< "$(echo "${LIST_OF_ROUTES}" | jq -r -c  '.url ')"
+    done
 
-	   c=${STATIC_START}
-	   for i in "${USED[@]}"; do 
-		   if [[ ${i} == 0 ]] ; then
-	              echo "${STATIC}${c}"
-	              #echo cf map-route ${ADD_ROUTE}  ${DOMAIN} --hostname ${STATIC}${c} 
-	              #echo cf set-env ${ADD_ROUTE} DFE_SIGNIN_BASE_URL https://${STATIC}${c}.${DOMAIN}
-	              #echo cf restage  ${ADD_ROUTE}
-                      break
-	           fi
-		   ((c+=1))
-	   done
+    # run through the array of static routes, looking for the first unused one
+    c=${STATIC_START}
+    for i in "${USED[@]}"; do
+        if [[ ${i} == 0 ]] ; then
+            echo "${STATIC}${c}"
+            break
+        fi
+        ((c+=1))
+    done
 fi
