77c1a974b83afe72da5f9cb99ae4c4eec52e20a8
Leonidas Apostolidis <leoapost@gmail.com>
2021-10-29 10:43:04 +0100

    Fix rubocop offenses

--- spec/services/bookings/reminder_spec.rb
+++ spec/services/bookings/reminder_spec.rb
@@ -5,13 +5,12 @@ describe Bookings::Reminder do
 
   let(:time_until_booking) { '3 weeks' }
   let(:school) { create(:bookings_school, :onboarded) }
-  let(:booking) { create(:bookings_booking, :accepted,  bookings_school: school, date: 3.weeks.from_now) }
+  let(:booking) { create(:bookings_booking, :accepted, bookings_school: school, date: 3.weeks.from_now) }
 
   subject { Bookings::Reminder.new(booking, time_until_booking) }
 
   describe '#deliver' do
     it "delivers one job per provided booking" do
-
       sign_up = build(:api_schools_experience_sign_up)
 
       expect_any_instance_of(GetIntoTeachingApiClient::SchoolsExperienceApi).to \
