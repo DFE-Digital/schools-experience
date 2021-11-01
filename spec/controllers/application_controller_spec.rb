1e80cbc5b09a69c3113c23b41bc717b92c1bca69
Leonidas Apostolidis <leoapost@users.noreply.github.com>
2021-11-01 14:56:24 +0000

    Set X-Robots-Tag header to all when visiting school profiles (#2069)

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -14,11 +14,24 @@ describe ApplicationController, type: :request do
       end
     end
 
+    context 'when school profile paths' do
+      let(:disabled_school) { create(:bookings_school, enabled: false) }
+
+      specify "should add 'X-Robots-Tag: all' for enabled schools" do
+        head candidates_school_path(school)
+        expect(response.headers[header]).to eql('all')
+      end
+
+      specify "should add 'X-Robots-Tag: none' for disabled schools" do
+        head candidates_school_path(disabled_school)
+        expect(response.headers[header]).to eql('none')
+      end
+    end
+
     specify "should add 'X-Robots-Tag: none' to uncrawlable paths" do
       [
         "/healthcheck.txt",
         candidates_schools_path,
-        candidates_school_path(school),
         new_candidates_school_registrations_background_check_path(school),
         new_candidates_school_registrations_personal_information_path(school)
 
