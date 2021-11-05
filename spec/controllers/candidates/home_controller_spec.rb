42583c657f3912913d01441a4ba51ee243a37132
Leonidas Apostolidis <leoapost@gmail.com>
2021-11-05 14:03:56 +0000

    Add meta description tags
    This commit installs the meta-tags gem and adds meta descriptions to the
    homepage and to the school profiles.

--- spec/controllers/candidates/home_controller_spec.rb
+++ spec/controllers/candidates/home_controller_spec.rb
@@ -2,9 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Candidates::HomeController, type: :request do
   describe "GET #index" do
-    it "returns http success with the Candidate landing page" do
+    before do
       get candidates_root_path
+    end
+
+    it "returns http success with the Candidate landing page" do
       expect(response).to have_http_status(:success)
+    end
+
+    it 'includes a meta description tag' do
+      expect(response.body).to include('<meta name="description" content="The Department for Education')
     end
   end
 end
