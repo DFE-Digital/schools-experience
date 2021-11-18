24c967f847f76725b783310707e44264d4651944
Joseph Kempster <jkempster34@gmail.com>
2021-11-18 11:52:33 +0000

    Re-enable buttons when navigating to page with browser back button
    We recently fixed some JavaScript that disables buttons so that a user can't submit a form multiple times.
    
    Howeve, pages don't reload fully when navigating back using the browser's back button, so the button remains disabled.
    
    We can add an event listener for the "pageshow" event to re-enable the button when this happens.

--- app/webpacker/controllers/prevent_double_click_controller.js
+++ app/webpacker/controllers/prevent_double_click_controller.js
@@ -3,6 +3,16 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static targets = [ 'submitButton' ]
 
+  connect() {
+    window.addEventListener( "pageshow", ( event ) => {
+      this.enableSubmitButton()
+    });
+  }
+
+  enableSubmitButton() {
+    this.submitButtonTarget.disabled = false;
+  }
+
   disableSubmitButton() {
     this.submitButtonTarget.disabled = true;
   };
