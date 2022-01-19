07a89c96f2175bdd611b46cd8314d5c0be724dc3
Ross Oliver <ross.oliver36@gmail.com>
2022-01-19 15:40:36 +0000

    Add Bookings::OutstandingTasks to easily summarise by school
    At the moment we calculate the outsanding tasks for schools in
    different places, using different methods. This has resulted in
    the badges on the dashboard not adding up to the total that is
    displayed on the school switcher page.
    
    Extract the logic from the dashboard and encapsulte in a new
    class that we can call and use in both scenarios.
    
    The current scope for the school switcher page is more optimal
    than this solution, but its impossible to try and reconcile the
    logic with the dashboard as it is. This will perform individuall
    requests that can then be totalled; I don't imagine the
    efficiency difference will be an issue and this will be far
    easier to maintain and debug.
    
    In my testing I occasionally got badges showing on the dashboard
    when there were no corresponding bookings/requests on the detail
    page. I think there is overlap between
    `requests_requiring_attention` and `unviewed_withdrawn_requests`
    at the moment, but that will be addressed in a later PR/commit.
    For now the aim is to keep it consistent with what the dashboard
    is displaying.v

---
+++ app/models/schools/outstanding_tasks.rb
@@ -0,0 +1,72 @@
+class Schools::OutstandingTasks
+  attr_reader :school_urns
+
+  def initialize(school_urns)
+    @school_urns = school_urns
+  end
+
+  def summarize
+    [
+      requests_requiring_attention,
+      unviewed_withdrawn_requests,
+      upcoming_bookings,
+      bookings_pending_attendance_confirmation
+    ].reduce(&:deep_merge!)
+  end
+
+  private
+
+  def requests_requiring_attention
+    query = schools
+      .joins(:placement_requests)
+      .merge(Bookings::PlacementRequest.requiring_attention)
+
+    keyed_total(query, :requests_requiring_attention)
+  end
+
+  def unviewed_withdrawn_requests
+    query = schools
+      .joins(:placement_requests)
+      .merge(Bookings::PlacementRequest.withdrawn_but_unviewed.reorder(nil))
+
+      keyed_total(query, :unviewed_withdrawn_requests)
+  end
+
+  def upcoming_bookings
+    query = schools
+      .joins(:bookings)
+      .merge(Bookings::Booking.with_unviewed_candidate_cancellation)
+
+      keyed_total(query, :upcoming_bookings)
+  end
+
+  def bookings_pending_attendance_confirmation
+    query = schools
+      .joins(:bookings)
+      .merge(Bookings::Booking.previous)
+      .merge(Bookings::Booking.not_cancelled)
+      .merge(Bookings::Booking.accepted)
+      .merge(Bookings::Booking.attendance_unlogged)
+
+      keyed_total(query, :bookings_pending_attendance_confirmation)
+  end
+
+  def schools
+    Bookings::School.where(urn: school_urns)
+  end
+
+  def keyed_total(query, key)
+    query
+      .group('bookings_schools.urn')
+      .count(:id)
+      .reduce(zero_hash(key)) do |hash, (urn, count)|
+        hash.tap { |h| h[urn] = { key => count } }
+      end
+  end
+
+  def zero_hash(key)
+    school_urns.reduce({}) do |hash, urn|
+      hash.tap { |h| h[urn] = Hash.new(0) }
+    end
+  end
+end
---
+++ spec/models/schools/outstanding_tasks_spec.rb
@@ -0,0 +1,135 @@
+require "rails_helper"
+
+describe Schools::OutstandingTasks do
+  let(:school) { create :bookings_school, :with_subjects }
+  let(:other_school) { create :bookings_school, :with_subjects }
+
+  describe "#summarize" do
+    let(:summary) { described_class.new([school.urn, other_school.urn]).summarize }
+
+    context "requests_requiring_attention" do
+      subject { compact_summary(:requests_requiring_attention) }
+
+      it { is_expected.to eq({ school.urn => 0, other_school.urn => 0 }) }
+
+      context "when there are requests requiring attention" do
+        before do
+          create(:placement_request, school: school)
+          create(:placement_request, :cancelled, school: school)
+        end
+
+        it { is_expected.to eq({ school.urn => 2, other_school.urn => 0 }) }
+
+        context "when there are also requests not requiring attention" do
+          before do
+            create(:placement_request, :booked, school: school)
+          end
+
+          it { is_expected.to eq({ school.urn => 2, other_school.urn => 0 }) }
+        end
+
+        context "when there are also other school requests requiring attention" do
+          before { create(:placement_request, school: other_school) }
+
+          it { is_expected.to eq({ school.urn => 2, other_school.urn => 1 }) }
+        end
+      end
+    end
+
+    context "unviewed_withdrawn_requests" do
+      subject { compact_summary(:unviewed_withdrawn_requests) }
+
+      it { is_expected.to eq({ school.urn => 0, other_school.urn => 0 }) }
+
+      context "when there are unviewed, withdrawn placement requests" do
+        before { create(:placement_request, :cancelled, school: school) }
+
+        it { is_expected.to eq({ school.urn => 1, other_school.urn => 0 }) }
+
+        context "when there are also viewed, withdrawn placement requests" do
+          before do
+            create(:placement_request, :cancelled, school: school).tap do |pr|
+              pr.candidate_cancellation.viewed!
+            end
+          end
+
+          it { is_expected.to eq({ school.urn => 1, other_school.urn => 0 }) }
+        end
+
+        context "when there are also other schools with unviewed, withdrawn placement requests" do
+          before { create(:placement_request, :cancelled, school: other_school) }
+
+          it { is_expected.to eq({ school.urn => 1, other_school.urn => 1 }) }
+        end
+      end
+    end
+
+    context "upcoming_bookings" do
+      subject { compact_summary(:upcoming_bookings) }
+
+      it { is_expected.to eq({ school.urn => 0, other_school.urn => 0 }) }
+
+      context "when there are bookings requiring attention" do
+        before { create(:bookings_booking, :cancelled_by_candidate, bookings_school: school) }
+
+        it { is_expected.to eq({ school.urn => 1, other_school.urn => 0 }) }
+
+        context "when there are also bookings not requiring attention" do
+          before do
+            create(:placement_request, :booked, school: school)
+            create(:bookings_booking, :with_viewed_candidate_cancellation, bookings_school: school)
+            create(:bookings_booking, :cancelled_by_school, bookings_school: school)
+          end
+
+          it { is_expected.to eq({ school.urn => 1, other_school.urn => 0 }) }
+        end
+
+        context "when there are also other schools with bookings requiring attention" do
+          before { create(:bookings_booking, :cancelled_by_candidate, bookings_school: other_school) }
+
+          it { is_expected.to eq({ school.urn => 1, other_school.urn => 1 }) }
+        end
+      end
+    end
+
+    context "bookings_pending_attendance_confirmation" do
+      subject { compact_summary(:bookings_pending_attendance_confirmation) }
+
+      it { is_expected.to eq({ school.urn => 0, other_school.urn => 0 }) }
+
+      context "when there are bookings pending attendance confirmation" do
+        before do
+          create(:placement_request, :booked, school: school) do |pr|
+            pr.booking.update_columns date: Date.yesterday
+          end
+        end
+
+        it { is_expected.to eq({ school.urn => 1, other_school.urn => 0 }) }
+
+        context "when there are also bookings not pending attendance confirmation" do
+          before do
+            create(:placement_request, :booked, school: school) do |pr|
+              pr.booking.update_columns date: Date.yesterday, attended: false
+            end
+          end
+
+          it { is_expected.to eq({ school.urn => 1, other_school.urn => 0 }) }
+        end
+
+        context "when there are also other schools with bookings pending attendance confirmation" do
+          before do
+            create(:placement_request, :booked, school: other_school) do |pr|
+              pr.booking.update_columns date: Date.yesterday
+            end
+          end
+
+          it { is_expected.to eq({ school.urn => 1, other_school.urn => 1 }) }
+        end
+      end
+    end
+  end
+
+  def compact_summary(key)
+    summary.transform_values { |v| v[key] }
+  end
+end
