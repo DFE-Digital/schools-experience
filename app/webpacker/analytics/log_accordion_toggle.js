80fdcfa2b05e5461038b415a24e1001eb071eb00
Ross Oliver <ross.oliver36@gmail.com>
2022-01-27 14:01:53 +0000

    Remove accordion GA event
    We don't appear to use this event and to avoid migrating it to the new GTM
    container I'm pulling it out.
    
    Also removes globally set `cookie_allowed` variable which doesn't appear to be
    referenced.

--- app/webpacker/analytics/log_accordion_toggle.js
+++
@@ -1,7 +0,0 @@
-import { sendGAEvent } from 'analytics/send_ga_event';
-
-const logAccordionToggle = (name, section, state) => {
-  sendGAEvent(name, section, state);
-};
-
-export { logAccordionToggle };
