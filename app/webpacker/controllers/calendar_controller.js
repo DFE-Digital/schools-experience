eaad5350a3fb14b6d0dee909668f69adfca76196
Joseph Kempster <jkempster34@gmail.com>
2022-03-25 10:55:05 +0000

    wip - add tags

--- app/webpacker/controllers/calendar_controller.js
+++ app/webpacker/controllers/calendar_controller.js
@@ -4,7 +4,8 @@ import '../stylesheets/calendar.scss';
 
 export default class extends Controller {
   static targets = [
-    "input"
+    "input",
+    "tagsWrapper"
   ];
 
   async connect() {
@@ -19,6 +20,12 @@ export default class extends Controller {
       },
       onDayCreate: (dateObject, dateString, flatpickr, dayElement) => {
         this.#addCloseButtonToSelected(dayElement)
+      },
+      onReady: (selectedDates, dateString, instance) => {
+        this.#addTags(selectedDates, instance)
+      },
+      onChange: (selectedDates, dateString, instance) => {
+        this.#addTags(selectedDates, instance)
       },
       disable: [this.#disableWeekends],
       mode: "multiple"
@@ -35,5 +42,43 @@ export default class extends Controller {
       closeButton.classList.add("close-button")
       dayElement.appendChild(closeButton)
     }
+  }
+
+  #addTags = (selectedDates, instance) => {
+    this.#clearTags();
+    const sortedDates = this.#sortDates(selectedDates)
+
+    sortedDates.forEach((date) => {
+      const tag = document.createElement("div")
+      tag.classList.add("tag")
+      tag.textContent = new Date(date).toLocaleDateString("en-GB", {day: "numeric", month: "short", year: "numeric"})
+      tag.addEventListener("click", (event) => this.#removeDate(event.target.textContent, instance, selectedDates))
+      this.tagsWrapperTarget.appendChild(tag)
+    })
+  }
+
+  #removeDate = (dateString, instance, selectedDates) => {
+    const dateToRemove = new Date(dateString);
+    instance.clear()
+
+    const restOfDates = selectedDates.filter(date => {
+      return date.getTime() !== dateToRemove.getTime()
+    })
+
+    this.#addTags(restOfDates, instance)
+    instance.setDate(restOfDates)
+    this.#jumpToMonth(instance, dateToRemove.getMonth())
+  }
+
+  #jumpToMonth = (instance, month) => {
+    instance.changeMonth(month, false)
+  }
+
+  #clearTags = () => {
+    this.tagsWrapperTarget.innerHTML = null
+  }
+
+  #sortDates = (dates) => {
+    return dates.sort((a, b) => a.getTime() - b.getTime());
   }
 }
