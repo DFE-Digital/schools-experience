9600b79631c783e5bb6c6ddd3bcf9e16ccc434c2
Ross Oliver <ross.oliver36@gmail.com>
2022-05-31 15:21:58 +0100

    Increase to use all 20 available DSi routes
    A while ago we added another 10 routes to DfE Sign In but neglected to
    update this script, which meant only 10 were being pulled instead of the
    full 20 and the pool was getting exhausted due to open dependabot PRs.
    
    Increase to use all available DSi routes.

--- script/get_next_mapping.sh
+++ script/get_next_mapping.sh
@@ -6,7 +6,7 @@ DOMAIN="london.cloudapps.digital"
 URL="https://api.london.cloud.service.gov.uk/v3/"
 STATIC="review-school-experience-"
 STATIC_START=1
-STATIC_END=10
+STATIC_END=20
 create=0
 
 # Get the token needed to run this
@@ -22,11 +22,11 @@ if [[ -z "${CURRENT}"  ]] ; then
    create=1
 else
    create=1
-   GUID=$(echo ${CURRENT}  | jq -r '.guid' ) 
+   GUID=$(echo ${CURRENT}  | jq -r '.guid' )
    LIST_OF_ROUTES=$(curl -s "${URL}/routes?app_guids=${GUID}"  -X GET  -H "Authorization: ${TOKEN}" | jq '.resources[] | {host,guid,url}')
    while read i; do
-      for (( v=${STATIC_START} ; v<${STATIC_END}+1 ; v++ )) 
-      do 
+      for (( v=${STATIC_START} ; v<${STATIC_END}+1 ; v++ ))
+      do
           if [[ "${i}" == "${STATIC}${v}" ]] ; then
 	      echo "${i}"
               create=0
@@ -42,8 +42,8 @@ if [[ ${create} == 1 ]]; then
 
            LIST_OF_ROUTES=$(curl -s "${URL}/routes?per_page=2000&app_guids=${CSV_LIST}"  -X GET  -H "Authorization: ${TOKEN}" | jq '.resources[] | {host,guid,url}')
 
-	   for (( v=${STATIC_START} ; v<${STATIC_END}+1 ; v++ )) 
-           do 
+	   for (( v=${STATIC_START} ; v<${STATIC_END}+1 ; v++ ))
+           do
               USED[$v]=0
               while read i; do
 		if [[ "${STATIC}${v}.${DOMAIN}" == ${i} ]]; then
@@ -53,10 +53,10 @@ if [[ ${create} == 1 ]]; then
            done
 
 	   c=${STATIC_START}
-	   for i in "${USED[@]}"; do 
+	   for i in "${USED[@]}"; do
 		   if [[ ${i} == 0 ]] ; then
 	              echo "${STATIC}${c}"
-	              #echo cf map-route ${ADD_ROUTE}  ${DOMAIN} --hostname ${STATIC}${c} 
+	              #echo cf map-route ${ADD_ROUTE}  ${DOMAIN} --hostname ${STATIC}${c}
 	              #echo cf set-env ${ADD_ROUTE} DFE_SIGNIN_BASE_URL https://${STATIC}${c}.${DOMAIN}
 	              #echo cf restage  ${ADD_ROUTE}
                       break
