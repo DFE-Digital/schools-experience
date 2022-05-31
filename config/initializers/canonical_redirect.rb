2ded1b088adfeaa2ee581573cff4919adbd47206
Ross Oliver <ross.oliver36@gmail.com>
2022-05-31 10:40:49 +0100

    Clean up environment variables
    Removes references to unused variables and
    adds descriptions where we are lacking.

--- config/initializers/canonical_redirect.rb
+++ config/initializers/canonical_redirect.rb
@@ -2,15 +2,6 @@ if ENV['CANONICAL_DOMAIN'].present? || Rails.env.test? || Rails.env.servertest?
   Rails.application.config.middleware.insert(0, Rack::Rewrite) do
     proto = Rails.application.config.force_ssl ? "https" : "http"
 
-    # If request via old domain pointing directly to Rails app
-    r302 %r{.*},
-      ->(_match, _rack_env) { "#{proto}://#{ENV['CANONICAL_DOMAIN']}/pages/migration" },
-      if: proc { |rack_env|
-        ENV['CANONICAL_DOMAIN'].present? &&
-          ENV['OLD_SEP_DOMAINS'].present? &&
-          rack_env['HTTP_HOST'].in?(ENV['OLD_SEP_DOMAINS'].split(',').compact)
-      }
-
     r302 %r{(.*)},
       ->(match, _rack_env) { "#{proto}://#{ENV['CANONICAL_DOMAIN']}#{match[1]}" },
       if: proc { |rack_env|
