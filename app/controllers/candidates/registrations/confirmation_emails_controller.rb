8b2d1845f049c0eae5ec7364ffae3dbd2536b280
Ross Oliver <ross.oliver36@gmail.com>
2023-01-16 11:32:22 +0000

    Remove confirmation requirement for new candidates
    Currently we require new candidates to confirm their email before issuing the
    placement request. We believe that this prevents a number of candidates from
    successfully finishing their request (the email doesn't arrive and/or they
    don't action it).
    
    Add feature flag to allow us to trial skipping the confirmation step. We will
    be adding other measures in order to reduce the risk of spam record creation.

--- app/controllers/candidates/registrations/confirmation_emails_controller.rb
+++ app/controllers/candidates/registrations/confirmation_emails_controller.rb
@@ -8,24 +8,38 @@ module Candidates
       end
 
       def create
+        sign_in_candidate if !candidate_signed_in? && Feature.enabled?(:skip_candidate_confirmation_emails)
+
         if candidate_signed_in?
           RegistrationStore.instance.store! current_registration
           redirect_to candidates_confirm_path uuid: current_registration.uuid
         else
-          current_registration.flag_as_pending_email_confirmation!
-          RegistrationStore.instance.store! current_registration
-
-          SendEmailConfirmationJob.perform_later \
-            current_registration.uuid,
-            request.host
-
-          redirect_to candidates_school_registrations_confirmation_email_path
+          send_confirmation_email
         end
       rescue RegistrationSession::NotCompletedError
         # We can get here if the school changes their date format while the
         # candidate is applying, or we push a code change mid way through a
         # registration.
         redirect_to next_step_path(current_registration)
+      end
+
+    private
+
+      def sign_in_candidate
+        self.current_candidate = Bookings::Candidate.create_or_update_from_registration_session! \
+          current_registration,
+          current_contact
+      end
+
+      def send_confirmation_email
+        current_registration.flag_as_pending_email_confirmation!
+        RegistrationStore.instance.store! current_registration
+
+        SendEmailConfirmationJob.perform_later \
+          current_registration.uuid,
+          request.host
+
+        redirect_to candidates_school_registrations_confirmation_email_path
       end
     end
   end
