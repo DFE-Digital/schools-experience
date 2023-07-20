4d15338ff88ad1c39f30897be8103f44df9ae257
Tope Emmanuel <topej.emmanuel@gmail.com>
2023-07-20 11:49:10 +0100

    Deploy redis
    WHY: Redis is a requisite componemt of the database deployment and it is hereby added to the IAC code in contimuation of the
         migration to AKS
    
    HOW: The existing code is  modified and customised for redis, allowing the new code to work
         side by side with the existing paas code

--- terraform/aks/database.tf
+++ terraform/aks/database.tf
@@ -1,17 +1,31 @@
 module "postgres" {
   source = "./vendor/modules/aks//aks/postgres"
 
-  namespace             = var.namespace
-  environment           = var.environment
-  azure_resource_prefix = var.azure_resource_prefix
-  service_name          = var.service_name
-  service_short         = var.service_short
-  config_short          = var.config_short
-
-  cluster_configuration_map = module.cluster_data.configuration_map
-
-  use_azure               = var.deploy_azure_backing_services
-  azure_enable_monitoring = false
-  server_version          = "14"
+  namespace                    = var.namespace
+  environment                  = var.environment
+  azure_resource_prefix        = var.azure_resource_prefix
+  service_name                 = var.service_name
+  service_short                = var.service_short
+  config_short                 = var.config_short
+  cluster_configuration_map    = module.cluster_data.configuration_map
+  use_azure                    = var.deploy_azure_backing_services
+  azure_enable_monitoring      = var.enable_monitoring
+  server_version               = "14"
   azure_enable_backup_storage  = var.azure_enable_backup_storage
+}
+
+module "redis-cache" {
+  source                       = "./vendor/modules/aks//aks/redis"
+
+  namespace                    = var.namespace
+  environment                  = var.environment
+  azure_resource_prefix        = var.azure_resource_prefix
+  service_short                = var.service_short
+  config_short                 = var.config_short
+  service_name                 = var.service_name
+  cluster_configuration_map    = module.cluster_data.configuration_map
+  use_azure                    = var.deploy_azure_backing_services
+  azure_enable_monitoring      = var.enable_monitoring
+  azure_patch_schedule         = [{ "day_of_week": "Sunday", "start_hour_utc": 01 }]
+  azure_maxmemory_policy       = "allkeys-lfu"
 }
