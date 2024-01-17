65deb1f72f36e2654c3c9b9f81d7a51b8b7753be
Chidi Ekuma <bukki.arise@yahoo.co.uk>
2024-01-17 20:20:30 +0000

    Update setup and webpack configuration

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,12 +1,5 @@
-// use the new NPM package name, `shakapacker`.
-// merge is webpack-merge from https://github.com/survivejs/webpack-merge
-const { generateWebpackConfig, merge } = require('shakapacker')
-const baseWebpackConfig = generateWebpackConfig()
+const { generateWebpackConfig } = require('shakapacker')
 
-const options = {
-  resolve: {
-    extensions: ['.scss', '.css', '.png', '.svg', '.gif', '.jpeg', '.jpg', 'ico', 'woff', 'woff2']
-  }
-}
+const webpackConfig = generateWebpackConfig()
 
-module.exports = merge({}, baseWebpackConfig, options)
+module.exports = webpackConfig
