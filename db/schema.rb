e1a1e12025bb10fcb2cc74c0b85d1d29d4113952
Chidi Ekuma <bukki.arise@yahoo.co.uk>
2024-02-27 16:46:59 +0000

    rollback db Due to ci failures; this is a temporal fix to to enable new build success. - Update schema version - Remove `:email`, `:last_name`, `:first_name` from users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_19_193536) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_04_080002) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "address_standardizer"
   enable_extension "plpgsql"
@@ -454,9 +454,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_19_193536) do
     t.string "sub"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
     t.index ["sub"], name: "index_users_on_sub", unique: true
   end
 
