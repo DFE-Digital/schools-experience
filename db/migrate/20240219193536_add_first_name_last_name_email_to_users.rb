120e66c55ec7393dd694f6b1666b57182652dc80
Chidi Ekuma <bukki.arise@yahoo.co.uk>
2024-02-27 17:04:19 +0000

    Remove migration file for adding first name, last name, and email to users table

--- db/migrate/20240219193536_add_first_name_last_name_email_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddFirstNameLastNameEmailToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :first_name, :string
-    add_column :users, :last_name, :string
-    add_column :users, :email, :string
-  end
-end
