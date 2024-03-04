f3bcb91d7280199ab9ba10679f34fb04d2ecf042
Chidi Ekuma <bukki.arise@yahoo.co.uk>
2024-03-04 20:00:46 +0000

    Add create action to users controller

--- app/controllers/schools/users_controller.rb
+++ app/controllers/schools/users_controller.rb
@@ -5,5 +5,22 @@ module Schools
     def new
       @user = DFESignInAPI::UserInvite.new
     end
+
+    def create
+      @user_invite = DfeSignInApi::UserInvite.new(user_params)
+
+      if @user_invite.valid?
+        @user_invite.invite_user
+        redirect_to users_path, notice: 'User invited successfully.'
+      else
+        render :new
+      end
+    end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:email, :first_name, :last_name, :organisation_id)
+    end
   end
 end
