c280eb6bbc6a3d0e3842c4c745fbda736ef49c6b
Ekuma Chidi <7931750+ekumachidi@users.noreply.github.com>
2024-03-11 19:16:03 +0000

    Invite Users to GSE (#3080)
    * Add users resource to `routes.rb`
    * Adds links to manage users on the school dashboard
    * Adds UsersController 
    * Adds views to manage users in a school
    * Adds DSI API service to fetch school's ukprn from DSI
    * Adds DSI API service to fetch users in a school
    * Adds DSI API service to add users to a school
    * Adds relevant specs

---
+++ app/controllers/schools/users_controller.rb
@@ -0,0 +1,44 @@
+module Schools
+  class UsersController < BaseController
+    def index
+      @users = DFESignInAPI::OrganisationUsers.new(current_user.sub, current_school.urn).users
+      @dfe_sign_in_request_organisation_url =
+        Rails.application.config.x.dfe_sign_in_request_organisation_url.presence
+    end
+
+    def new
+      @user_invite = DFESignInAPI::UserInvite.new
+    end
+
+    def create
+      @user_invite = DFESignInAPI::UserInvite.new(user_params)
+      @user_invite.organisation_id = DFESignInAPI::Organisation.new(current_user.sub, current_school.urn).current_organisation_id
+
+      if params[:confirmed] == 'true'
+        if @user_invite.valid?
+          @user_invite.create
+          redirect_to schools_users_path, notice: "#{@user_invite.email} has been added."
+        else
+          render :new
+        end
+      else
+        render :show, locals: { user_invite: @user_invite }
+      end
+    end
+
+    def show
+      render :show
+    end
+
+    def edit
+      @user_invite = DFESignInAPI::UserInvite.new(user_params)
+      render :new, locals: { user_invite: @user_invite }
+    end
+
+  private
+
+    def user_params
+      params.require(:schools_dfe_sign_in_api_user_invite).permit(:email, :firstname, :lastname, :organisation_id)
+    end
+  end
+end
---
+++ app/services/schools/dfe_sign_in_api/user_invite.rb
@@ -0,0 +1,59 @@
+module Schools
+  module DFESignInAPI
+    class UserInvite < Client
+      include ActiveModel::Model
+      include ActiveModel::Attributes
+      include ActiveRecord::AttributeAssignment
+
+      attribute :email, :string
+      attribute :firstname, :string
+      attribute :lastname, :string
+      attribute :organisation_id, :string
+
+      validates :firstname, presence: true, length: { maximum: 50 }
+      validates :lastname, presence: true, length: { maximum: 50 }
+      validates :email, presence: true, length: { maximum: 100 }
+      validates :email, email_format: true, if: -> { email.present? }
+      validates :organisation_id, presence: true
+
+      def create
+        @response = response
+      end
+
+    private
+
+      def response
+        raise ApiDisabled unless enabled?
+
+        resp = faraday.post(endpoint) do |req|
+          req.headers['Authorization'] = "bearer #{token}"
+          req.headers['Content-Type'] = 'application/json'
+          req.body = payload.to_json
+        end
+
+        resp.body
+      end
+
+      def service_id
+        ENV.fetch('DFE_SIGNIN_SCHOOL_EXPERIENCE_ADMIN_SERVICE_ID')
+      end
+
+      def endpoint
+        URI::HTTPS.build(
+          host: Rails.configuration.x.dfe_sign_in_api_host,
+          path: ['/services', service_id, 'invitations'].join('/')
+        )
+      end
+
+      def payload
+        super.merge(
+          sourceId: SecureRandom.uuid,
+          given_name: firstname,
+          family_name: lastname,
+          email: email,
+          organisation: organisation_id
+        )
+      end
+    end
+  end
+end
