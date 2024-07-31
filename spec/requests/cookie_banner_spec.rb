074bb8af8d15a8273e7ebbafb7f058d6fe1959c3
Spencer Dixon <spencerlloyddixon@gmail.com>
2024-07-31 09:53:05 +0100

    Remove unused seen_cookie_message spec (#3244)

--- spec/requests/cookie_banner_spec.rb
+++ spec/requests/cookie_banner_spec.rb
@@ -1,16 +1,9 @@
 require 'rails_helper'
 
 describe "Displaying the cookie banner", type: :request do
-  let(:cookie_name) { 'seen_cookie_message' }
   let(:banner_contents) { /Cookies on the Get school experience service/ }
 
   context 'on the first visit (when the seen_cookie_message cookie is absent)' do
-    specify "the cookie should not be set to 'yes'" do
-      expect(cookies[cookie_name]).to be_nil
-      get candidates_root_path
-      expect(cookies[cookie_name]).to be_nil
-    end
-
     specify 'the page should display the cookie banner' do
       get candidates_root_path
       expect(response.body).to match(banner_contents)
