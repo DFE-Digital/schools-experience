17c4c29a7c7814a269c7b97d3b7d7475e0afaf71
David Young <david@dcyoung.dev>
2026-09-25 12:04:19 +0100

    Ignore rubocop rule

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,7 +4,9 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 if Rails.env.production? || Rails.env.servertest? || Rails.env.staging?
+  # rubocop:disable Rails/Exit
   abort("The Rails environment is running in production mode!")
+  # rubocop:enable Rails/Exit
 end
 
 require 'rspec/rails'
@@ -35,7 +37,9 @@ end
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
+  # rubocop:disable Rails/Exit
   abort("There are pending migrations: #{e.to_s.strip}")
+  # rubocop:enable Rails/Exit
 end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
